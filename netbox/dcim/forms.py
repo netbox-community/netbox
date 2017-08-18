2c339b1bda205cdfe6fb878b8f4fe90338f4737f
Joseph Kennedy <joseph@netsiphon.com>
2017-08-17 21:32:30 -0400

    Comment mac_address and enabled in InterfaceListFilterForm

--- netbox/dcim/forms.py
+++ netbox/dcim/forms.py
@@ -1574,12 +1574,12 @@ class InterfaceFilterForm(BootstrapMixin, forms.Form):
 class InterfaceListFilterForm(BootstrapMixin, forms.Form):
     site = forms.ModelChoiceField(required=False, queryset=Site.objects.all(), to_field_name='slug')
     device = forms.ModelChoiceField(required=False, queryset=Device.objects.all(), to_field_name='slug')
-    enabled = forms.ChoiceField(choices=add_blank_choice(IFACE_ENABLED_CHOICES), required=False)
+    #enabled = forms.ChoiceField(choices=add_blank_choice(IFACE_ENABLED_CHOICES), required=False)
     #role = FilterChoiceField(
     #    queryset=DeviceRole.objects.annotate(filter_count=Count('devices')),
     #    to_field_name='slug',
     #)
-    mac_address = forms.CharField(required=False, label='MAC address')
+    #mac_address = forms.CharField(required=False, label='MAC address')
 
 
 #
