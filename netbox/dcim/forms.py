b9060d846c126bdcd71967b8fb3fa4fd23fda375
Joseph Kennedy <joseph@netsiphon.com>
2017-08-26 23:35:46 -0400

    Try again...

--- netbox/dcim/forms.py
+++ netbox/dcim/forms.py
@@ -1607,7 +1607,7 @@ class InterfaceCSVForm(forms.ModelForm):
                 device=device_id, form_factor=IFACE_FF_LAG).get(
                 lag=lag_name.id
             )
-        if not self.data('lag'):
+        if lag is not None:
             return None
         return lag
 
