d534fa8e4991da2423c5d056f6c0d58eebceb844
Joseph Kennedy <joseph@netsiphon.com>
2017-08-30 19:51:54 -0400

    Fix syntax

--- netbox/dcim/filters.py
+++ netbox/dcim/filters.py
@@ -659,7 +659,7 @@ class InterfaceListFilter(django_filters.FilterSet):
         if not value.strip():
             return queryset
         return queryset.filter(
-            Q(mac_address__icontains=value) |
+            Q(mac_address__icontains=value)
         ).distinct()
 
 
