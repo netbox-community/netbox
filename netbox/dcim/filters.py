53cbaffc5d7ab8697ac81dc8f9210d0e7e7a53b7
Joseph Kennedy <joseph@netsiphon.com>
2017-08-30 19:51:10 -0400

    Leave only mac_address in search...

--- netbox/dcim/filters.py
+++ netbox/dcim/filters.py
@@ -659,9 +659,7 @@ class InterfaceListFilter(django_filters.FilterSet):
         if not value.strip():
             return queryset
         return queryset.filter(
-            Q(device__icontains=value.strip()) |
-            Q(name__icontains=value.strip()) |
-            Q(mac_address__icontains=value)
+            Q(mac_address__icontains=value) |
         ).distinct()
 
 
