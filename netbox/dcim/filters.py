0625caa296a9c65f0a7c1d4a197db8b0f152a11b
Joseph Kennedy <joseph@netsiphon.com>
2017-08-30 21:37:59 -0400

    Select_related again...

--- netbox/dcim/filters.py
+++ netbox/dcim/filters.py
@@ -583,12 +583,12 @@ class InterfaceListFilter(django_filters.FilterSet):
             label='Search',
     )
     site_id = django_filters.ModelMultipleChoiceFilter(
-        queryset=Site.objects.all(),
+        queryset=Site.objects.select.related('site_id','device_id__site'),
         label='Site (ID)',
     )
     site = django_filters.ModelMultipleChoiceFilter(
         name='site__slug',
-        queryset=Site.objects.all(),
+        queryset=Site.objects.select_related('site_id','device_id__site'),
         to_field_name='slug',
         label='Site name (slug)',
     )
