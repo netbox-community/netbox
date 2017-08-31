ded6ca7c0c394176c2680b2f6b5f3e64f7d288aa
Joseph Kennedy <joseph@netsiphon.com>
2017-08-30 21:42:31 -0400

    Remove device_id

--- netbox/dcim/filters.py
+++ netbox/dcim/filters.py
@@ -583,12 +583,12 @@ class InterfaceListFilter(django_filters.FilterSet):
             label='Search',
     )
     site_id = django_filters.ModelMultipleChoiceFilter(
-        queryset=Device.objects.select_related('site','device_id__site').all(),
+        queryset=Device.objects.select_related('site').all(),
         label='Site (ID)',
     )
     site = django_filters.ModelMultipleChoiceFilter(
         name='site__slug',
-        queryset=Device.objects.select_related('site','device_id__site').all(),
+        queryset=Device.objects.select_related('site').all(),
         to_field_name='slug',
         label='Site name (slug)',
     )
