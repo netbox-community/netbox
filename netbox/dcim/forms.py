d87c88a23d98872a567ff660db94d4611e708fce
Joseph Kennedy <joseph@netsiphon.com>
2017-08-31 16:58:51 -0400

    Change required to false for rack and rack group

--- netbox/dcim/forms.py
+++ netbox/dcim/forms.py
@@ -1621,13 +1621,15 @@ class InterfaceListFilterForm(BootstrapMixin, forms.Form):
     q = forms.CharField(required=False, label='Search')
     site = forms.ModelChoiceField(required=False, queryset=Site.objects.all(), to_field_name='slug')
     rack_group_id = FilterChoiceField(
+        required=False,
         queryset=RackGroup.objects.select_related('site').annotate(filter_count=Count('racks__devices')),
-        label='Rack group',
+        label='Rack group'
     )
     rack_id = FilterChoiceField(
+        required=False,
         queryset=Rack.objects.annotate(filter_count=Count('devices')),
         label='Rack',
-        null_option=(0, 'None'),
+        null_option=(0, 'None')
     )
     enabled = forms.ChoiceField(choices=add_blank_choice(IFACE_ENABLED_CHOICES), required=False)
     role = FilterChoiceField(
