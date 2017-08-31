b2ef26249e614a369ce9a55366d0fc4592f4c344
Joseph Kennedy <joseph@netsiphon.com>
2017-08-30 21:07:48 -0400

    Add name property to filters

--- netbox/dcim/filters.py
+++ netbox/dcim/filters.py
@@ -593,18 +593,22 @@ class InterfaceListFilter(django_filters.FilterSet):
     )
     site_id = django_filters.ModelMultipleChoiceFilter(
         method='_filter_site',
+        name='site',
         label='Site (ID)',
     )
     site = django_filters.ModelMultipleChoiceFilter(
         method='_filter_site',
+        name='site',
         label='Site name (slug)',
     )
     role_id = django_filters.ModelMultipleChoiceFilter(
         method='_filter_role',
+        name='device_role',
         label='Role (ID)',
     )
     role = django_filters.ModelMultipleChoiceFilter(
         method='_filter_role',
+        name='device_role',
         label='Role (slug)',
     )
     type = django_filters.CharFilter(
