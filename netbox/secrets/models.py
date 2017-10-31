1a61941ed3278fcc2bfba1675524c7b3d063b825
Tom Strickx <tstrickx@cloudflare.com>
2017-10-31 10:28:14 -0700

    Adapted PR to comply with comments
    Re-introduced certain references to make sure explicit references are still used where needed.

--- netbox/secrets/models.py
+++ netbox/secrets/models.py
@@ -288,6 +288,9 @@ class Secret(CreatedUpdatedModel):
     def __str__(self):
         if self.role and self.device and self.name:
             return '{} for {} ({})'.format(self.role, self.device, self.name)
+        # Return role and device if no name is set
+        if self.role and self.device:
+            return '{} for {}'.format(self.role, self.device)
         return 'Secret'
 
     def get_absolute_url(self):
