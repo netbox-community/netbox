ca2b0dd91c9e03380bced271fb73633a07bb5e69
Josh VanDeraa <joshvanderaa@gmail.com>
2020-12-27 16:21:52 +0000

    Fixes inconsistent return; adds discrete arguments

--- netbox/dcim/models/racks.py
+++ netbox/dcim/models/racks.py
@@ -562,7 +562,7 @@ class Rack(ChangeLoggedModel, CustomFieldModel):
         if power_stats:
             allocated_draw_total = sum(x['allocated_draw_total'] or 0 for x in power_stats)
             available_power_total = sum(x['available_power'] for x in power_stats)
-            return int(allocated_draw_total / available_power_total * 100) or (0, 0)
+            return (allocated_draw_total, available_power_total) or (0, 0)
         return (0, 0)
 
 
