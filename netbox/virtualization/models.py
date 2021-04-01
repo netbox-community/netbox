7ebd9a4e51670ac197f69cd5b4e00e53c3b32fe7
Sammy Chafiqui <joezeppe.code@gmail.com>
2021-03-31 20:04:41 -0400

    #5822: Increase Virtual Machine Model Name Length to 255

--- netbox/virtualization/models.py
+++ netbox/virtualization/models.py
@@ -223,7 +223,7 @@ class VirtualMachine(ChangeLoggedModel, ConfigContextModel, CustomFieldModel):
         null=True
     )
     name = models.CharField(
-        max_length=64
+        max_length=255
     )
     status = models.CharField(
         max_length=50,
