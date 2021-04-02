netbox-community/netbox

commit 7e20beae7e9af22114a4e165fc1c828084128857

Remove brief_mode parameter from DynamicModelChoiceMixin

netbox/project-static/js/forms.js
@@ -160,9 +160,6 @@ $(document).ready(function() {
                     offset: offset,
                 };
 
-                // Allow for controlling the brief setting from within APISelect
-                parameters.brief = ( $(element).is('[data-full]') ? undefined : true );
-
                 // Attach any extra query parameters
                 $.each(element.attributes, function(index, attr){
                     if (attr.name.includes("data-query-param-")){

netbox/utilities/forms/fields.py
@@ -271,20 +271,18 @@ class DynamicModelChoiceMixin:
     :param null_option: The string used to represent a null selection (if any)
     :param disabled_indicator: The name of the field which, if populated, will disable selection of the
         choice (optional)
-    :param brief_mode: Use the "brief" format (?brief=true) when making API requests (default)
     """
     filter = django_filters.ModelChoiceFilter
     widget = widgets.APISelect
 
     # TODO: Remove display_field in v2.12
     def __init__(self, display_field='display', query_params=None, initial_params=None, null_option=None,
-                 disabled_indicator=None, brief_mode=True, *args, **kwargs):
+                 disabled_indicator=None, *args, **kwargs):
         self.display_field = display_field
         self.query_params = query_params or {}
         self.initial_params = initial_params or {}
         self.null_option = null_option
         self.disabled_indicator = disabled_indicator
-        self.brief_mode = brief_mode
 
         # to_field_name is set by ModelChoiceField.__init__(), but we need to set it early for reference
         # by widget_attrs()
@@ -308,10 +306,6 @@ class DynamicModelChoiceMixin:
         # Set the disabled indicator, if any
         if self.disabled_indicator is not None:
             attrs['disabled-indicator'] = self.disabled_indicator
-
-        # Toggle brief mode
-        if not self.brief_mode:
-            attrs['data-full'] = 'true'
 
         # Attach any static query parameters
         for key, value in self.query_params.items():

commit b05fcfae76e671e3e34d29b3d303d79157472de9

Restore 'brief' parameter (regression from 7e20beae7e9af22114a4e165fc1c828084128857)

netbox/project-static/js/forms.js
@@ -158,6 +158,7 @@ $(document).ready(function() {
                     q: params.term,
                     limit: 50,
                     offset: offset,
+                    brief: true,
                 };
 
                 // Attach any extra query parameters