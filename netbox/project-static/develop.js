aecd2d22aeef24a210b78a93261cafa64bf08122
ndom91 <yo@ndo.dev>
2021-09-05 00:09:54 +0200

    fix: output

--- netbox/project-static/develop.js
+++ netbox/project-static/develop.js
@@ -5,18 +5,18 @@ const sassWatcher = chokidar.watch('styles/**/*.scss');
 const tsWatcher = chokidar.watch('src/**/*.ts');
 
 const collectStatic = type => {
-  console.log('[*] bundling...');
+  console.log('[*] bundling..');
   let args = '';
   if (type === 'style') args += '--styles';
   if (type === 'script') args += '--scripts';
   execSync(`node bundle.js ${args}`);
 
-  console.log('[*] collecting...');
+  console.log('[*] collecting..');
   exec('../../venv/bin/python3 ../manage.py collectstatic --no-input', (err, stdout, stderr) => {
     err && console.error(err);
     stdout && console.log(`[*] ${stdout.trim()}`);
     stderr && console.log('[**] Python Err:', stderr);
-    console.log('[*] waiting...\n');
+    console.log('[*] waiting..\n');
   });
 };
 
