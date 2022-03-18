d3774801c45d1060309d1b06df109923bda94aeb
Pieter Lambrecht <pieter.lambrecht@sentia.com>
2022-03-18 09:42:33 +0100

    Remove betteradminfield Move IP verification to authenticate()

--- netbox/netbox/api/authentication.py
+++ netbox/netbox/api/authentication.py
@@ -10,11 +10,24 @@ class TokenAuthentication(authentication.TokenAuthentication):
     A custom authentication scheme which enforces Token expiration times.
     """
     model = Token
-    __request = False
 
     def authenticate(self, request):
-        self.request = request
-        return super().authenticate(request)
+        token_user, token = super().authenticate(request)
+
+        # Verify source IP is allowed
+        if token.allowed_ips:
+            # Replace 'HTTP_X_REAL_IP' with the settings variable choosen in #8867
+            if 'HTTP_X_REAL_IP' in request.META:
+                clientip = request.META['HTTP_X_REAL_IP'].split(",")[0].strip()
+            elif 'REMOTE_ADDR' in request.META:
+                clientip = request.META['REMOTE_ADDR']
+            else:
+                raise exceptions.AuthenticationFailed(f"A HTTP header containing the SourceIP (HTTP_X_REAL_IP, REMOTE_ADDR) is missing from the request.")
+
+            if not token.validate_client_ip(clientip):
+                raise exceptions.AuthenticationFailed(f"Source IP {clientip} is not allowed to use this token.")
+
+        return token_user, token
 
     def authenticate_credentials(self, key):
         model = self.get_model()
@@ -22,20 +35,6 @@ class TokenAuthentication(authentication.TokenAuthentication):
             token = model.objects.prefetch_related('user').get(key=key)
         except model.DoesNotExist:
             raise exceptions.AuthenticationFailed("Invalid token")
-
-        # Verify source IP is allowed
-        request = self.request
-        if token.allowed_ips and request:
-            # Replace 'HTTP_X_REAL_IP' with the settings variable choosen in #8867
-            if 'HTTP_X_REAL_IP' in request.META:
-                clientip = request.META['HTTP_X_REAL_IP'].split(",")[0].strip()
-            elif 'REMOTE_ADDR' in request.META:
-                clientip = request.META['REMOTE_ADDR']
-            else:
-                raise exceptions.AuthenticationFailed(f"The request HTTP headers (HTTP_X_REAL_IP, REMOTE_ADDR) are missing or do not contain a valid source IP.")
-
-            if not token.validate_client_ip(clientip):
-                raise exceptions.AuthenticationFailed(f"Source IP {clientip} is not allowed to use this token.")
 
         # Enforce the Token's expiration time, if one has been set.
         if token.is_expired:
--- netbox/users/admin/__init__.py
+++ netbox/users/admin/__init__.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as UserAdmin_
 from django.contrib.auth.models import Group, User
-from django_better_admin_arrayfield.admin.mixins import DynamicArrayMixin
 
 from users.models import ObjectPermission, Token
 from . import filters, forms, inlines
@@ -56,7 +55,7 @@ class UserAdmin(UserAdmin_):
 #
 
 @admin.register(Token)
-class TokenAdmin(admin.ModelAdmin, DynamicArrayMixin):
+class TokenAdmin(admin.ModelAdmin):
     form = forms.TokenAdminForm
     list_display = [
         'key', 'user', 'created', 'expires', 'write_enabled', 'description', 'list_allowed_ips'
