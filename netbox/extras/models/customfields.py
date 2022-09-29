1f0daa24f73fad75fbcd419fea716bb4c06fa978
jeremystretch <jstretch@ns1.com>
2022-09-29 10:23:10 -0400

    Clean up validation logic

--- netbox/extras/models/customfields.py
+++ netbox/extras/models/customfields.py
@@ -220,14 +220,11 @@ class CustomField(CloningMixin, ExportTemplatesMixin, WebhooksMixin, ChangeLogge
                 })
 
         # Minimum/maximum values can be set only for numeric fields
-        if self.validation_minimum is not None and (self.type != CustomFieldTypeChoices.TYPE_INTEGER and self.type != CustomFieldTypeChoices.TYPE_DECIMAL):
-            raise ValidationError({
-                'validation_minimum': "A minimum value may be set only for numeric fields"
-            })
-        if self.validation_maximum is not None and (self.type != CustomFieldTypeChoices.TYPE_INTEGER and self.type != CustomFieldTypeChoices.TYPE_DECIMAL):
-            raise ValidationError({
-                'validation_maximum': "A maximum value may be set only for numeric fields"
-            })
+        if self.type not in (CustomFieldTypeChoices.TYPE_INTEGER, CustomFieldTypeChoices.TYPE_DECIMAL):
+            if self.validation_minimum:
+                raise ValidationError({'validation_minimum': "A minimum value may be set only for numeric fields"})
+            if self.validation_maximum:
+                raise ValidationError({'validation_maximum': "A maximum value may be set only for numeric fields"})
 
         # Regex validation can be set only for text fields
         regex_types = (
