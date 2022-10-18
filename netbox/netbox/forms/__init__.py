f69a13e05b7e956b7c66ec3369033c3ede0711f2
jeremystretch <jstretch@ns1.com>
2022-10-18 08:17:35 -0400

    Remove object type selector from search bar

--- netbox/netbox/forms/__init__.py
+++ netbox/netbox/forms/__init__.py
@@ -15,21 +15,10 @@ LOOKUP_CHOICES = (
 )
 
 
-def build_options(choices):
-    options = [{"label": choices[0][1], "items": []}]
-
-    for label, choices in choices[1:]:
-        items = []
-
-        for value, choice_label in choices:
-            items.append({"label": choice_label, "value": value})
-
-        options.append({"label": label, "items": items})
-    return options
-
-
 class SearchForm(BootstrapMixin, forms.Form):
-    q = forms.CharField(label='Search')
+    q = forms.CharField(
+        label='Search'
+    )
     obj_types = forms.MultipleChoiceField(
         choices=[],
         required=False,
@@ -43,15 +32,7 @@ class SearchForm(BootstrapMixin, forms.Form):
         widget=StaticSelect()
     )
 
-    options = None
-
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         self.fields['obj_types'].choices = search_backend.get_object_types()
-
-    def get_options(self):
-        if not self.options:
-            self.options = build_options(search_backend.get_object_types())
-
-        return self.options
