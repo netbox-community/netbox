1135019d0244992c312bb10305b729f490ee140a
Arthur Hanson <worldnomad@gmail.com>
2022-11-17 16:46:30 -0800

    Update netbox/users/signals.py
    Co-authored-by: Jeremy Stretch <jstretch@ns1.com>

--- netbox/users/signals.py
+++ netbox/users/signals.py
@@ -6,5 +6,5 @@ from django.contrib.auth.signals import user_login_failed
 @receiver(user_login_failed)
 def log_user_login_failed(sender, credentials, request, **kwargs):
     logger = logging.getLogger('netbox.auth.login')
-    username = credentials.get("username", None)
+    username = credentials.get("username")
     logger.info(f"Failed login attempt for username: {username}")
