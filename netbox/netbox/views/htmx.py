48cb2036e96a4f3eecec7fa90275dc3331fb8512
jeremystretch <jstretch@netboxlabs.com>
2023-03-03 09:22:32 -0500

    WIP

--- netbox/netbox/views/htmx.py
+++ netbox/netbox/views/htmx.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import View
 
+from dcim.filtersets import SiteFilterSet
 from dcim.forms import SiteFilterForm
+from dcim.models import Site
 
 
 class ObjectSelectorView(View):
@@ -11,10 +13,31 @@ class ObjectSelectorView(View):
         form_class = self._get_form_class()
         form = form_class(request.GET)
 
+        if '_search' in request.GET:
+            # Return only search results
+            model = self._get_model()
+            filterset = self._get_filterset_class()
+
+            queryset = model.objects.restrict(request.user)
+            if filterset:
+                queryset = filterset(request.GET, queryset, request=request).qs
+
+            return render(request, 'htmx/object_selector_results.html', {
+                'results': queryset,
+            })
+
         return render(request, self.template_name, {
             'form': form,
         })
 
+    def _get_model(self):
+        # TODO: Determine model from request parameters
+        return Site
+
     def _get_form_class(self):
-        # TODO: Determine form class from request parameters
+        # TODO: Determine form class from model
         return SiteFilterForm
+
+    def _get_filterset_class(self):
+        # TODO: Determine filterset class from model
+        return SiteFilterSet
