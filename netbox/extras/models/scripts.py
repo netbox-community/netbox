f4fe7a1f9e414969ff8df3e80d8b2895940dab77
jeremystretch <jstretch@netboxlabs.com>
2023-03-29 11:39:29 -0400

    Catch ImportErrors when rendering report/script lists

--- netbox/extras/models/scripts.py
+++ netbox/extras/models/scripts.py
@@ -51,7 +51,10 @@ class ScriptModule(PythonModuleMixin, JobsMixin, ManagedFile):
             # For child objects in submodules use the full import path w/o the root module as the name
             return cls.full_name.split(".", maxsplit=1)[1]
 
-        module = self.get_module()
+        try:
+            module = self.get_module()
+        except ImportError:
+            return {}
         scripts = {}
         ordered = getattr(module, 'script_order', [])
 
