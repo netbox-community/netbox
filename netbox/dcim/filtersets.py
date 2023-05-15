19b7a20e8016889f2299b19bea0bc3d819f7ffb2
Arthur <worldnomad@gmail.com>
2023-05-15 13:11:21 -0700

    12489 add has_position filter

--- netbox/dcim/filtersets.py
+++ netbox/dcim/filtersets.py
@@ -909,6 +909,10 @@ class DeviceFilterSet(NetBoxModelFilterSet, TenancyFilterSet, ContactModelFilter
         queryset=Rack.objects.all(),
         label=_('Rack (ID)'),
     )
+    has_position = django_filters.BooleanFilter(
+        label=_('Has a position'),
+        method='_has_position'
+    )
     cluster_id = django_filters.ModelMultipleChoiceFilter(
         queryset=Cluster.objects.all(),
         label=_('VM cluster (ID)'),
@@ -1049,6 +1053,9 @@ class DeviceFilterSet(NetBoxModelFilterSet, TenancyFilterSet, ContactModelFilter
 
     def _device_bays(self, queryset, name, value):
         return queryset.exclude(devicebays__isnull=value)
+
+    def _has_position(self, queryset, name, value):
+        return queryset.exclude(position__isnull=value)
 
 
 class VirtualDeviceContextFilterSet(NetBoxModelFilterSet, TenancyFilterSet):
