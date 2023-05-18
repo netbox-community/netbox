121e5c2c91b25629340e9767edeb2509267a9e3b
Jonathan Senecal <jonathan.senecal@metrooptic.com>
2023-05-18 10:53:41 -0400

    Hide grid stack buttons when dashboard is locked

--- netbox/project-static/src/dashboard.ts
+++ netbox/project-static/src/dashboard.ts
@@ -6,8 +6,13 @@ function lockDashboard(): void {
   const dashboard = document.getElementById('dashboard') as any;
   const gridUnlockButton = document.getElementById('unlock_dashboard') as HTMLButtonElement;
   const gridLockButton = document.getElementById('lock_dashboard') as HTMLButtonElement;
+
   if (dashboard) {
     dashboard.gridstack.disable();
+    // Hide the grid stack items buttons
+    Array.from(document.getElementsByClassName("grid-stack-item-button")).forEach(function (element) {
+      element.classList.add('invisible');
+    });
   }
   if (gridUnlockButton && gridLockButton) {
     gridUnlockButton.classList.remove('invisible');
@@ -21,6 +26,10 @@ function unlockDashboard(): void {
   const gridLockButton = document.getElementById('lock_dashboard') as HTMLButtonElement;
   if (dashboard) {
     dashboard.gridstack.enable();
+    // Show the grid stack items buttons
+    Array.from(document.getElementsByClassName("grid-stack-item-button")).forEach(function (element) {
+      element.classList.remove('invisible');
+    });
   }
   if (gridUnlockButton && gridLockButton) {
     gridUnlockButton.classList.add('invisible');
