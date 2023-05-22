17d68c4390ac7aaf80e3bb54d0f8a36a16430bdc
jeremystretch <jstretch@netboxlabs.com>
2023-05-22 14:48:50 -0400

    Replace 'null' with FILTERS_NULL_CHOICE_VALUE constant

--- netbox/dcim/forms/bulk_edit.py
+++ netbox/dcim/forms/bulk_edit.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.conf import settings
 from django.contrib.auth.models import User
 from django.utils.translation import gettext as _
 from timezone_field import TimeZoneFormField
@@ -1293,8 +1294,12 @@ class InterfaceBulkEditForm(
 
                 if site is not None:
                     # Query for VLANs assigned to the same site and VLANs with no site assigned (null).
-                    self.fields['untagged_vlan'].widget.add_query_param('site_id', [site.pk, 'null'])
-                    self.fields['tagged_vlans'].widget.add_query_param('site_id', [site.pk, 'null'])
+                    self.fields['untagged_vlan'].widget.add_query_param(
+                        'site_id', [site.pk, settings.FILTERS_NULL_CHOICE_VALUE]
+                    )
+                    self.fields['tagged_vlans'].widget.add_query_param(
+                        'site_id', [site.pk, settings.FILTERS_NULL_CHOICE_VALUE]
+                    )
 
             self.fields['parent'].choices = ()
             self.fields['parent'].widget.attrs['disabled'] = True
