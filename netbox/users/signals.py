07c3b178c798cca4e20a09fb38974f1cc2dfc093
Abhimanyu Saharan <desk.abhimanyu@gmail.com>
2023-05-26 01:48:38 +0530

    adds ip to failed logs #12562

--- netbox/users/signals.py
+++ netbox/users/signals.py
@@ -1,10 +1,11 @@
 import logging
 from django.dispatch import receiver
 from django.contrib.auth.signals import user_login_failed
+from utilities.request import get_client_ip
 
 
 @receiver(user_login_failed)
 def log_user_login_failed(sender, credentials, request, **kwargs):
     logger = logging.getLogger('netbox.auth.login')
     username = credentials.get("username")
-    logger.info(f"Failed login attempt for username: {username}")
+    logger.info(f"Failed login attempt for username: {username} from {get_client_ip(request)}")
