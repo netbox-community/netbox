b438a3a3977c5e8c2b0b5c7517548fccc468073f
Daniel W. Anner <daniel.anner@danstechsupport.com>
2023-07-25 19:42:56 +0000

    Adding interface `TYPE_100GE_SFP_DD`/`100gbase-x-sfpdd`

--- netbox/dcim/choices.py
+++ netbox/dcim/choices.py
@@ -810,6 +810,7 @@ class InterfaceTypeChoices(ChoiceSet):
     TYPE_100GE_CXP = '100gbase-x-cxp'
     TYPE_100GE_CPAK = '100gbase-x-cpak'
     TYPE_100GE_DSFP = '100gbase-x-dsfp'
+    TYPE_100GE_SFP_DD = '100gbase-x-sfpdd'
     TYPE_100GE_QSFP28 = '100gbase-x-qsfp28'
     TYPE_100GE_QSFP_DD = '100gbase-x-qsfpdd'
     TYPE_200GE_CFP2 = '200gbase-x-cfp2'
@@ -961,6 +962,7 @@ class InterfaceTypeChoices(ChoiceSet):
                 (TYPE_100GE_CXP, 'CXP (100GE)'),
                 (TYPE_100GE_CPAK, 'Cisco CPAK (100GE)'),
                 (TYPE_100GE_DSFP, 'DSFP (100GE)'),
+                (TYPE_100GE_SFP_DD, "SPF-DD (100GE)"),
                 (TYPE_100GE_QSFP28, 'QSFP28 (100GE)'),
                 (TYPE_100GE_QSFP_DD, 'QSFP-DD (100GE)'),
                 (TYPE_200GE_QSFP56, 'QSFP56 (200GE)'),
