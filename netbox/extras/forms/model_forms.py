AmazeCom/nautobot

13149 add gettext_lazy to forms

netbox/extras/forms/model_forms.py
@@ -4,7 +4,7 @@ from django import forms
 from django.conf import settings
 from django.db.models import Q
 from django.contrib.contenttypes.models import ContentType
-from django.utils.translation import gettext as _
+from django.utils.translation import gettext_lazy as _
 
 from core.forms.mixins import SyncedDataMixin
 from dcim.models import DeviceRole, DeviceType, Location, Platform, Region, Site, SiteGroup
@@ -40,10 +40,12 @@ __all__ = (
 
 class CustomFieldForm(BootstrapMixin, forms.ModelForm):
     content_types = ContentTypeMultipleChoiceField(
+        label=_('Content types'),
         queryset=ContentType.objects.all(),
         limit_choices_to=FeatureQuery('custom_fields'),
     )
     object_type = ContentTypeChoiceField(
+        label=_('Object type'),
         queryset=ContentType.objects.all(),
         # TODO: Come up with a canonical way to register suitable models
         limit_choices_to=FeatureQuery('webhooks').get_query() | Q(app_label='auth', model__in=['user', 'group']),
@@ -52,12 +54,12 @@ class CustomFieldForm(BootstrapMixin, forms.ModelForm):
     )
 
     fieldsets = (
-        ('Custom Field', (
+        (_('Custom Field'), (
             'content_types', 'name', 'label', 'group_name', 'type', 'object_type', 'required', 'description',
         )),
-        ('Behavior', ('search_weight', 'filter_logic', 'ui_visibility', 'weight', 'is_cloneable')),
-        ('Values', ('default', 'choices')),
-        ('Validation', ('validation_minimum', 'validation_maximum', 'validation_regex')),
+        (_('Behavior'), ('search_weight', 'filter_logic', 'ui_visibility', 'weight', 'is_cloneable')),
+        (_('Values'), ('default', 'choices')),
+        (_('Validation'), ('validation_minimum', 'validation_maximum', 'validation_regex')),
     )
 
     class Meta:
@@ -80,13 +82,14 @@ class CustomFieldForm(BootstrapMixin, forms.ModelForm):
 
 class CustomLinkForm(BootstrapMixin, forms.ModelForm):
     content_types = ContentTypeMultipleChoiceField(
+        label=_('Content types'),
         queryset=ContentType.objects.all(),
         limit_choices_to=FeatureQuery('custom_links')
     )
 
     fieldsets = (
-        ('Custom Link', ('name', 'content_types', 'weight', 'group_name', 'button_class', 'enabled', 'new_window')),
-        ('Templates', ('link_text', 'link_url')),
+        (_('Custom Link'), ('name', 'content_types', 'weight', 'group_name', 'button_class', 'enabled', 'new_window')),
+        (_('Templates'), ('link_text', 'link_url')),
     )
 
     class Meta:
@@ -107,18 +110,20 @@ class CustomLinkForm(BootstrapMixin, forms.ModelForm):
 
 class ExportTemplateForm(BootstrapMixin, SyncedDataMixin, forms.ModelForm):
     content_types = ContentTypeMultipleChoiceField(
+        label=_('Content types'),
         queryset=ContentType.objects.all(),
         limit_choices_to=FeatureQuery('export_templates')
     )
     template_code = forms.CharField(
+        label=_('Template code'),
         required=False,
         widget=forms.Textarea(attrs={'class': 'font-monospace'})
     )
 
     fieldsets = (
-        ('Export Template', ('name', 'content_types', 'description', 'template_code')),
-        ('Data Source', ('data_source', 'data_file', 'auto_sync_enabled')),
-        ('Rendering', ('mime_type', 'file_extension', 'as_attachment')),
+        (_('Export Template'), ('name', 'content_types', 'description', 'template_code')),
+        (_('Data Source'), ('data_source', 'data_file', 'auto_sync_enabled')),
+        (_('Rendering'), ('mime_type', 'file_extension', 'as_attachment')),
     )
 
     class Meta:
@@ -139,7 +144,7 @@ class ExportTemplateForm(BootstrapMixin, SyncedDataMixin, forms.ModelForm):
         super().clean()
 
         if not self.cleaned_data.get('template_code') and not self.cleaned_data.get('data_file'):
-            raise forms.ValidationError("Must specify either local content or a data file")
+            raise forms.ValidationError(_("Must specify either local content or a data file"))
 
         return self.cleaned_data
 
@@ -147,13 +152,14 @@ class ExportTemplateForm(BootstrapMixin, SyncedDataMixin, forms.ModelForm):
 class SavedFilterForm(BootstrapMixin, forms.ModelForm):
     slug = SlugField()
     content_types = ContentTypeMultipleChoiceField(
+        label=_('Content types'),
         queryset=ContentType.objects.all()
     )
     parameters = JSONField()
 
     fieldsets = (
-        ('Saved Filter', ('name', 'slug', 'content_types', 'description', 'weight', 'enabled', 'shared')),
-        ('Parameters', ('parameters',)),
+        (_('Saved Filter'), ('name', 'slug', 'content_types', 'description', 'weight', 'enabled', 'shared')),
+        (_('Parameters'), ('parameters',)),
     )
 
     class Meta:
@@ -172,6 +178,7 @@ class SavedFilterForm(BootstrapMixin, forms.ModelForm):
 
 class BookmarkForm(BootstrapMixin, forms.ModelForm):
     object_type = ContentTypeChoiceField(
+        label=_('Object type'),
         queryset=ContentType.objects.all(),
         limit_choices_to=FeatureQuery('bookmarks').get_query()
     )
@@ -183,29 +190,30 @@ class BookmarkForm(BootstrapMixin, forms.ModelForm):
 
 class WebhookForm(BootstrapMixin, forms.ModelForm):
     content_types = ContentTypeMultipleChoiceField(
+        label=_('Content types'),
         queryset=ContentType.objects.all(),
         limit_choices_to=FeatureQuery('webhooks')
     )
 
     fieldsets = (
-        ('Webhook', ('name', 'content_types', 'enabled')),
-        ('Events', ('type_create', 'type_update', 'type_delete', 'type_job_start', 'type_job_end')),
-        ('HTTP Request', (
+        (_('Webhook'), ('name', 'content_types', 'enabled')),
+        (_('Events'), ('type_create', 'type_update', 'type_delete', 'type_job_start', 'type_job_end')),
+        (_('HTTP Request'), (
             'payload_url', 'http_method', 'http_content_type', 'additional_headers', 'body_template', 'secret',
         )),
-        ('Conditions', ('conditions',)),
-        ('SSL', ('ssl_verification', 'ca_file_path')),
+        (_('Conditions'), ('conditions',)),
+        (_('SSL'), ('ssl_verification', 'ca_file_path')),
     )
 
     class Meta:
         model = Webhook
         fields = '__all__'
         labels = {
-            'type_create': 'Creations',
-            'type_update': 'Updates',
-            'type_delete': 'Deletions',
-            'type_job_start': 'Job executions',
-            'type_job_end': 'Job terminations',
+            'type_create': _('Creations'),
+            'type_update': _('Updates'),
+            'type_delete': _('Deletions'),
+            'type_job_start': _('Job executions'),
+            'type_job_end': _('Job terminations'),
         }
         widgets = {
             'additional_headers': forms.Textarea(attrs={'class': 'font-monospace'}),
@@ -217,6 +225,7 @@ class WebhookForm(BootstrapMixin, forms.ModelForm):
 class TagForm(BootstrapMixin, forms.ModelForm):
     slug = SlugField()
     object_types = ContentTypeMultipleChoiceField(
+        label=_('Object types'),
         queryset=ContentType.objects.all(),
         limit_choices_to=FeatureQuery('tags'),
         required=False
@@ -235,65 +244,79 @@ class TagForm(BootstrapMixin, forms.ModelForm):
 
 class ConfigContextForm(BootstrapMixin, SyncedDataMixin, forms.ModelForm):
     regions = DynamicModelMultipleChoiceField(
+        label=_('Regions'),
         queryset=Region.objects.all(),
         required=False
     )
     site_groups = DynamicModelMultipleChoiceField(
+        label=_('Site groups'),
         queryset=SiteGroup.objects.all(),
         required=False
     )
     sites = DynamicModelMultipleChoiceField(
+        label=_('Sites'),
         queryset=Site.objects.all(),
         required=False
     )
     locations = DynamicModelMultipleChoiceField(
+        label=_('Locations'),
         queryset=Location.objects.all(),
         required=False
     )
     device_types = DynamicModelMultipleChoiceField(
+        label=_('Device types'),
         queryset=DeviceType.objects.all(),
         required=False
     )
     roles = DynamicModelMultipleChoiceField(
+        label=_('Roles'),
         queryset=DeviceRole.objects.all(),
         required=False
     )
     platforms = DynamicModelMultipleChoiceField(
+        label=_('Platforms'),
         queryset=Platform.objects.all(),
         required=False
     )
     cluster_types = DynamicModelMultipleChoiceField(
+        label=_('Cluster types'),
         queryset=ClusterType.objects.all(),
         required=False
     )
     cluster_groups = DynamicModelMultipleChoiceField(
+        label=_('Cluster groups'),
         queryset=ClusterGroup.objects.all(),
         required=False
     )
     clusters = DynamicModelMultipleChoiceField(
+        label=_('Clusters'),
         queryset=Cluster.objects.all(),
         required=False
     )
     tenant_groups = DynamicModelMultipleChoiceField(
+        label=_('Tenat groups'),
         queryset=TenantGroup.objects.all(),
         required=False
     )
     tenants = DynamicModelMultipleChoiceField(
+        label=_('Tenants'),
         queryset=Tenant.objects.all(),
         required=False
     )
     tags = DynamicModelMultipleChoiceField(
+        label=_('Tags'),
         queryset=Tag.objects.all(),
         required=False
     )
     data = JSONField(
+        label=_('Data'),
         required=False
     )
 
     fieldsets = (
-        ('Config Context', ('name', 'weight', 'description', 'data', 'is_active')),
-        ('Data Source', ('data_source', 'data_file', 'auto_sync_enabled')),
-        ('Assignment', (
+        (_('Config Context'), ('name', 'weight', 'description', 'data', 'is_active')),
+        (_('Data Source'), ('data_source', 'data_file', 'auto_sync_enabled')),
+        (_('Assignment'), (
             'regions', 'site_groups', 'sites', 'locations', 'device_types', 'roles', 'platforms', 'cluster_types',
             'cluster_groups', 'clusters', 'tenant_groups', 'tenants', 'tags',
         )),
@@ -325,25 +348,27 @@ class ConfigContextForm(BootstrapMixin, SyncedDataMixin, forms.ModelForm):
         super().clean()
 
         if not self.cleaned_data.get('data') and not self.cleaned_data.get('data_file'):
-            raise forms.ValidationError("Must specify either local data or a data file")
+            raise forms.ValidationError(_("Must specify either local data or a data file"))
 
         return self.cleaned_data
 
 
 class ConfigTemplateForm(BootstrapMixin, SyncedDataMixin, forms.ModelForm):
     tags = DynamicModelMultipleChoiceField(
+        label=_('Tags'),
         queryset=Tag.objects.all(),
         required=False
     )
     template_code = forms.CharField(
+        label=_('Template code'),
         required=False,
         widget=forms.Textarea(attrs={'class': 'font-monospace'})
     )
 
     fieldsets = (
-        ('Config Template', ('name', 'description', 'environment_params', 'tags')),
-        ('Content', ('template_code',)),
-        ('Data Source', ('data_source', 'data_file', 'auto_sync_enabled')),
+        (_('Config Template'), ('name', 'description', 'environment_params', 'tags')),
+        (_('Content'), ('template_code',)),
+        (_('Data Source'), ('data_source', 'data_file', 'auto_sync_enabled')),
     )
 
     class Meta:
@@ -367,7 +392,7 @@ class ConfigTemplateForm(BootstrapMixin, SyncedDataMixin, forms.ModelForm):
         super().clean()
 
         if not self.cleaned_data.get('template_code') and not self.cleaned_data.get('data_file'):
-            raise forms.ValidationError("Must specify either local content or a data file")
+            raise forms.ValidationError(_("Must specify either local content or a data file"))
 
         return self.cleaned_data
 
@@ -383,10 +408,13 @@ class ImageAttachmentForm(BootstrapMixin, forms.ModelForm):
 
 class JournalEntryForm(NetBoxModelForm):
     kind = forms.ChoiceField(
+        label=_('Kind'),
         choices=add_blank_choice(JournalEntryKindChoices),
         required=False
     )
-    comments = CommentField()
+    comments = CommentField(
+        label=_('Comments'),
+    )
 
     class Meta:
         model = JournalEntry
@@ -425,16 +453,16 @@ class ConfigRevisionForm(BootstrapMixin, forms.ModelForm, metaclass=ConfigFormMe
     """
 
     fieldsets = (
-        ('Rack Elevations', ('RACK_ELEVATION_DEFAULT_UNIT_HEIGHT', 'RACK_ELEVATION_DEFAULT_UNIT_WIDTH')),
-        ('Power', ('POWERFEED_DEFAULT_VOLTAGE', 'POWERFEED_DEFAULT_AMPERAGE', 'POWERFEED_DEFAULT_MAX_UTILIZATION')),
-        ('IPAM', ('ENFORCE_GLOBAL_UNIQUE', 'PREFER_IPV4')),
-        ('Security', ('ALLOWED_URL_SCHEMES',)),
-        ('Banners', ('BANNER_LOGIN', 'BANNER_MAINTENANCE', 'BANNER_TOP', 'BANNER_BOTTOM')),
-        ('Pagination', ('PAGINATE_COUNT', 'MAX_PAGE_SIZE')),
-        ('Validation', ('CUSTOM_VALIDATORS',)),
-        ('User Preferences', ('DEFAULT_USER_PREFERENCES',)),
-        ('Miscellaneous', ('MAINTENANCE_MODE', 'GRAPHQL_ENABLED', 'CHANGELOG_RETENTION', 'JOB_RETENTION', 'MAPS_URL')),
-        ('Config Revision', ('comment',))
+        (_('Rack Elevations'), ('RACK_ELEVATION_DEFAULT_UNIT_HEIGHT', 'RACK_ELEVATION_DEFAULT_UNIT_WIDTH')),
+        (_('Power'), ('POWERFEED_DEFAULT_VOLTAGE', 'POWERFEED_DEFAULT_AMPERAGE', 'POWERFEED_DEFAULT_MAX_UTILIZATION')),
+        (_('IPAM'), ('ENFORCE_GLOBAL_UNIQUE', 'PREFER_IPV4')),
+        (_('Security'), ('ALLOWED_URL_SCHEMES',)),
+        (_('Banners'), ('BANNER_LOGIN', 'BANNER_MAINTENANCE', 'BANNER_TOP', 'BANNER_BOTTOM')),
+        (_('Pagination'), ('PAGINATE_COUNT', 'MAX_PAGE_SIZE')),
+        (_('Validation'), ('CUSTOM_VALIDATORS',)),
+        (_('User Preferences'), ('DEFAULT_USER_PREFERENCES',)),
+        (_('Miscellaneous'), ('MAINTENANCE_MODE', 'GRAPHQL_ENABLED', 'CHANGELOG_RETENTION', 'JOB_RETENTION', 'MAPS_URL')),
+        (_('Config Revision'), ('comment',))
     )
 
     class Meta:
@@ -461,11 +489,11 @@ class ConfigRevisionForm(BootstrapMixin, forms.ModelForm, metaclass=ConfigFormMe
                 help_text = self.fields[param.name].help_text
                 if help_text:
                     help_text += '<br />'  # Line break
-                help_text += f'Current value: <strong>{value}</strong>'
+                help_text += _('Current value: <strong>{value}</strong>').format(value=value)
                 if is_static:
-                    help_text += ' (defined statically)'
+                    help_text += _(' (defined statically)')
                 elif value == param.default:
-                    help_text += ' (default)'
+                    help_text += _(' (default)')
                 self.fields[param.name].help_text = help_text
                 self.fields[param.name].initial = value
             if is_static:

netbox-community/netbox

netbox/extras/forms/model_forms.py
@@ -4,7 +4,7 @@ from django import forms
 from django.conf import settings
 from django.db.models import Q
 from django.contrib.contenttypes.models import ContentType
-from django.utils.translation import gettext as _
+from django.utils.translation import gettext_lazy as _
 
 from core.forms.mixins import SyncedDataMixin
 from dcim.models import DeviceRole, DeviceType, Location, Platform, Region, Site, SiteGroup
@@ -42,10 +42,12 @@ __all__ = (
 
 class CustomFieldForm(BootstrapMixin, forms.ModelForm):
     content_types = ContentTypeMultipleChoiceField(
+        label=_('Content types'),
         queryset=ContentType.objects.all(),
         limit_choices_to=FeatureQuery('custom_fields'),
     )
     object_type = ContentTypeChoiceField(
+        label=_('Object type'),
         queryset=ContentType.objects.all(),
         # TODO: Come up with a canonical way to register suitable models
         limit_choices_to=FeatureQuery('webhooks').get_query() | Q(app_label='auth', model__in=['user', 'group']),
@@ -58,12 +60,12 @@ class CustomFieldForm(BootstrapMixin, forms.ModelForm):
     )
 
     fieldsets = (
-        ('Custom Field', (
+        (_('Custom Field'), (
             'content_types', 'name', 'label', 'group_name', 'type', 'object_type', 'required', 'description',
         )),
-        ('Behavior', ('search_weight', 'filter_logic', 'ui_visibility', 'weight', 'is_cloneable')),
-        ('Values', ('default', 'choice_set')),
-        ('Validation', ('validation_minimum', 'validation_maximum', 'validation_regex')),
+        (_('Behavior'), ('search_weight', 'filter_logic', 'ui_visibility', 'weight', 'is_cloneable')),
+        (_('Values'), ('default', 'choice_set')),
+        (_('Validation'), ('validation_minimum', 'validation_maximum', 'validation_regex')),
     )
 
     class Meta:
@@ -100,13 +102,14 @@ class CustomFieldChoiceSetForm(BootstrapMixin, forms.ModelForm):
 
 class CustomLinkForm(BootstrapMixin, forms.ModelForm):
     content_types = ContentTypeMultipleChoiceField(
+        label=_('Content types'),
         queryset=ContentType.objects.all(),
         limit_choices_to=FeatureQuery('custom_links')
     )
 
     fieldsets = (
-        ('Custom Link', ('name', 'content_types', 'weight', 'group_name', 'button_class', 'enabled', 'new_window')),
-        ('Templates', ('link_text', 'link_url')),
+        (_('Custom Link'), ('name', 'content_types', 'weight', 'group_name', 'button_class', 'enabled', 'new_window')),
+        (_('Templates'), ('link_text', 'link_url')),
     )
 
     class Meta:
@@ -127,18 +130,20 @@ class CustomLinkForm(BootstrapMixin, forms.ModelForm):
 
 class ExportTemplateForm(BootstrapMixin, SyncedDataMixin, forms.ModelForm):
     content_types = ContentTypeMultipleChoiceField(
+        label=_('Content types'),
         queryset=ContentType.objects.all(),
         limit_choices_to=FeatureQuery('export_templates')
     )
     template_code = forms.CharField(
+        label=_('Template code'),
         required=False,
         widget=forms.Textarea(attrs={'class': 'font-monospace'})
     )
 
     fieldsets = (
-        ('Export Template', ('name', 'content_types', 'description', 'template_code')),
-        ('Data Source', ('data_source', 'data_file', 'auto_sync_enabled')),
-        ('Rendering', ('mime_type', 'file_extension', 'as_attachment')),
+        (_('Export Template'), ('name', 'content_types', 'description', 'template_code')),
+        (_('Data Source'), ('data_source', 'data_file', 'auto_sync_enabled')),
+        (_('Rendering'), ('mime_type', 'file_extension', 'as_attachment')),
     )
 
     class Meta:
@@ -159,7 +164,7 @@ class ExportTemplateForm(BootstrapMixin, SyncedDataMixin, forms.ModelForm):
         super().clean()
 
         if not self.cleaned_data.get('template_code') and not self.cleaned_data.get('data_file'):
-            raise forms.ValidationError("Must specify either local content or a data file")
+            raise forms.ValidationError(_("Must specify either local content or a data file"))
 
         return self.cleaned_data
 
@@ -167,13 +172,14 @@ class ExportTemplateForm(BootstrapMixin, SyncedDataMixin, forms.ModelForm):
 class SavedFilterForm(BootstrapMixin, forms.ModelForm):
     slug = SlugField()
     content_types = ContentTypeMultipleChoiceField(
+        label=_('Content types'),
         queryset=ContentType.objects.all()
     )
     parameters = JSONField()
 
     fieldsets = (
-        ('Saved Filter', ('name', 'slug', 'content_types', 'description', 'weight', 'enabled', 'shared')),
-        ('Parameters', ('parameters',)),
+        (_('Saved Filter'), ('name', 'slug', 'content_types', 'description', 'weight', 'enabled', 'shared')),
+        (_('Parameters'), ('parameters',)),
     )
 
     class Meta:
@@ -192,6 +198,7 @@ class SavedFilterForm(BootstrapMixin, forms.ModelForm):
 
 class BookmarkForm(BootstrapMixin, forms.ModelForm):
     object_type = ContentTypeChoiceField(
+        label=_('Object type'),
         queryset=ContentType.objects.all(),
         limit_choices_to=FeatureQuery('bookmarks').get_query()
     )
@@ -203,29 +210,30 @@ class BookmarkForm(BootstrapMixin, forms.ModelForm):
 
 class WebhookForm(BootstrapMixin, forms.ModelForm):
     content_types = ContentTypeMultipleChoiceField(
+        label=_('Content types'),
         queryset=ContentType.objects.all(),
         limit_choices_to=FeatureQuery('webhooks')
     )
 
     fieldsets = (
-        ('Webhook', ('name', 'content_types', 'enabled')),
-        ('Events', ('type_create', 'type_update', 'type_delete', 'type_job_start', 'type_job_end')),
-        ('HTTP Request', (
+        (_('Webhook'), ('name', 'content_types', 'enabled')),
+        (_('Events'), ('type_create', 'type_update', 'type_delete', 'type_job_start', 'type_job_end')),
+        (_('HTTP Request'), (
             'payload_url', 'http_method', 'http_content_type', 'additional_headers', 'body_template', 'secret',
         )),
-        ('Conditions', ('conditions',)),
-        ('SSL', ('ssl_verification', 'ca_file_path')),
+        (_('Conditions'), ('conditions',)),
+        (_('SSL'), ('ssl_verification', 'ca_file_path')),
     )
 
     class Meta:
         model = Webhook
         fields = '__all__'
         labels = {
-            'type_create': 'Creations',
-            'type_update': 'Updates',
-            'type_delete': 'Deletions',
-            'type_job_start': 'Job executions',
-            'type_job_end': 'Job terminations',
+            'type_create': _('Creations'),
+            'type_update': _('Updates'),
+            'type_delete': _('Deletions'),
+            'type_job_start': _('Job executions'),
+            'type_job_end': _('Job terminations'),
         }
         widgets = {
             'additional_headers': forms.Textarea(attrs={'class': 'font-monospace'}),
@@ -237,6 +245,7 @@ class WebhookForm(BootstrapMixin, forms.ModelForm):
 class TagForm(BootstrapMixin, forms.ModelForm):
     slug = SlugField()
     object_types = ContentTypeMultipleChoiceField(
+        label=_('Object types'),
         queryset=ContentType.objects.all(),
         limit_choices_to=FeatureQuery('tags'),
         required=False
@@ -255,65 +264,79 @@ class TagForm(BootstrapMixin, forms.ModelForm):
 
 class ConfigContextForm(BootstrapMixin, SyncedDataMixin, forms.ModelForm):
     regions = DynamicModelMultipleChoiceField(
+        label=_('Regions'),
         queryset=Region.objects.all(),
         required=False
     )
     site_groups = DynamicModelMultipleChoiceField(
+        label=_('Site groups'),
         queryset=SiteGroup.objects.all(),
         required=False
     )
     sites = DynamicModelMultipleChoiceField(
+        label=_('Sites'),
         queryset=Site.objects.all(),
         required=False
     )
     locations = DynamicModelMultipleChoiceField(
+        label=_('Locations'),
         queryset=Location.objects.all(),
         required=False
     )
     device_types = DynamicModelMultipleChoiceField(
+        label=_('Device types'),
         queryset=DeviceType.objects.all(),
         required=False
     )
     roles = DynamicModelMultipleChoiceField(
+        label=_('Roles'),
         queryset=DeviceRole.objects.all(),
         required=False
     )
     platforms = DynamicModelMultipleChoiceField(
+        label=_('Platforms'),
         queryset=Platform.objects.all(),
         required=False
     )
     cluster_types = DynamicModelMultipleChoiceField(
+        label=_('Cluster types'),
         queryset=ClusterType.objects.all(),
         required=False
     )
     cluster_groups = DynamicModelMultipleChoiceField(
+        label=_('Cluster groups'),
         queryset=ClusterGroup.objects.all(),
         required=False
     )
     clusters = DynamicModelMultipleChoiceField(
+        label=_('Clusters'),
         queryset=Cluster.objects.all(),
         required=False
     )
     tenant_groups = DynamicModelMultipleChoiceField(
+        label=_('Tenat groups'),
         queryset=TenantGroup.objects.all(),
         required=False
     )
     tenants = DynamicModelMultipleChoiceField(
+        label=_('Tenants'),
         queryset=Tenant.objects.all(),
         required=False
     )
     tags = DynamicModelMultipleChoiceField(
+        label=_('Tags'),
         queryset=Tag.objects.all(),
         required=False
     )
     data = JSONField(
+        label=_('Data'),
         required=False
     )
 
     fieldsets = (
-        ('Config Context', ('name', 'weight', 'description', 'data', 'is_active')),
-        ('Data Source', ('data_source', 'data_file', 'auto_sync_enabled')),
-        ('Assignment', (
+        (_('Config Context'), ('name', 'weight', 'description', 'data', 'is_active')),
+        (_('Data Source'), ('data_source', 'data_file', 'auto_sync_enabled')),
+        (_('Assignment'), (
             'regions', 'site_groups', 'sites', 'locations', 'device_types', 'roles', 'platforms', 'cluster_types',
             'cluster_groups', 'clusters', 'tenant_groups', 'tenants', 'tags',
         )),
@@ -345,25 +368,27 @@ class ConfigContextForm(BootstrapMixin, SyncedDataMixin, forms.ModelForm):
         super().clean()
 
         if not self.cleaned_data.get('data') and not self.cleaned_data.get('data_file'):
-            raise forms.ValidationError("Must specify either local data or a data file")
+            raise forms.ValidationError(_("Must specify either local data or a data file"))
 
         return self.cleaned_data
 
 
 class ConfigTemplateForm(BootstrapMixin, SyncedDataMixin, forms.ModelForm):
     tags = DynamicModelMultipleChoiceField(
+        label=_('Tags'),
         queryset=Tag.objects.all(),
         required=False
     )
     template_code = forms.CharField(
+        label=_('Template code'),
         required=False,
         widget=forms.Textarea(attrs={'class': 'font-monospace'})
     )
 
     fieldsets = (
-        ('Config Template', ('name', 'description', 'environment_params', 'tags')),
-        ('Content', ('template_code',)),
-        ('Data Source', ('data_source', 'data_file', 'auto_sync_enabled')),
+        (_('Config Template'), ('name', 'description', 'environment_params', 'tags')),
+        (_('Content'), ('template_code',)),
+        (_('Data Source'), ('data_source', 'data_file', 'auto_sync_enabled')),
     )
 
     class Meta:
@@ -387,7 +412,7 @@ class ConfigTemplateForm(BootstrapMixin, SyncedDataMixin, forms.ModelForm):
         super().clean()
 
         if not self.cleaned_data.get('template_code') and not self.cleaned_data.get('data_file'):
-            raise forms.ValidationError("Must specify either local content or a data file")
+            raise forms.ValidationError(_("Must specify either local content or a data file"))
 
         return self.cleaned_data
 
@@ -403,10 +428,13 @@ class ImageAttachmentForm(BootstrapMixin, forms.ModelForm):
 
 class JournalEntryForm(NetBoxModelForm):
     kind = forms.ChoiceField(
+        label=_('Kind'),
         choices=add_blank_choice(JournalEntryKindChoices),
         required=False
     )
-    comments = CommentField()
+    comments = CommentField(
+        label=_('Comments'),
+    )
 
     class Meta:
         model = JournalEntry
@@ -445,16 +473,16 @@ class ConfigRevisionForm(BootstrapMixin, forms.ModelForm, metaclass=ConfigFormMe
     """
 
     fieldsets = (
-        ('Rack Elevations', ('RACK_ELEVATION_DEFAULT_UNIT_HEIGHT', 'RACK_ELEVATION_DEFAULT_UNIT_WIDTH')),
-        ('Power', ('POWERFEED_DEFAULT_VOLTAGE', 'POWERFEED_DEFAULT_AMPERAGE', 'POWERFEED_DEFAULT_MAX_UTILIZATION')),
-        ('IPAM', ('ENFORCE_GLOBAL_UNIQUE', 'PREFER_IPV4')),
-        ('Security', ('ALLOWED_URL_SCHEMES',)),
-        ('Banners', ('BANNER_LOGIN', 'BANNER_MAINTENANCE', 'BANNER_TOP', 'BANNER_BOTTOM')),
-        ('Pagination', ('PAGINATE_COUNT', 'MAX_PAGE_SIZE')),
-        ('Validation', ('CUSTOM_VALIDATORS',)),
-        ('User Preferences', ('DEFAULT_USER_PREFERENCES',)),
-        ('Miscellaneous', ('MAINTENANCE_MODE', 'GRAPHQL_ENABLED', 'CHANGELOG_RETENTION', 'JOB_RETENTION', 'MAPS_URL')),
-        ('Config Revision', ('comment',))
+        (_('Rack Elevations'), ('RACK_ELEVATION_DEFAULT_UNIT_HEIGHT', 'RACK_ELEVATION_DEFAULT_UNIT_WIDTH')),
+        (_('Power'), ('POWERFEED_DEFAULT_VOLTAGE', 'POWERFEED_DEFAULT_AMPERAGE', 'POWERFEED_DEFAULT_MAX_UTILIZATION')),
+        (_('IPAM'), ('ENFORCE_GLOBAL_UNIQUE', 'PREFER_IPV4')),
+        (_('Security'), ('ALLOWED_URL_SCHEMES',)),
+        (_('Banners'), ('BANNER_LOGIN', 'BANNER_MAINTENANCE', 'BANNER_TOP', 'BANNER_BOTTOM')),
+        (_('Pagination'), ('PAGINATE_COUNT', 'MAX_PAGE_SIZE')),
+        (_('Validation'), ('CUSTOM_VALIDATORS',)),
+        (_('User Preferences'), ('DEFAULT_USER_PREFERENCES',)),
+        (_('Miscellaneous'), ('MAINTENANCE_MODE', 'GRAPHQL_ENABLED', 'CHANGELOG_RETENTION', 'JOB_RETENTION', 'MAPS_URL')),
+        (_('Config Revision'), ('comment',))
     )
 
     class Meta:
@@ -481,11 +509,11 @@ class ConfigRevisionForm(BootstrapMixin, forms.ModelForm, metaclass=ConfigFormMe
                 help_text = self.fields[param.name].help_text
                 if help_text:
                     help_text += '<br />'  # Line break
-                help_text += f'Current value: <strong>{value}</strong>'
+                help_text += _('Current value: <strong>{value}</strong>').format(value=value)
                 if is_static:
-                    help_text += ' (defined statically)'
+                    help_text += _(' (defined statically)')
                 elif value == param.default:
-                    help_text += ' (default)'
+                    help_text += _(' (default)')
                 self.fields[param.name].help_text = help_text
                 self.fields[param.name].initial = value
             if is_static: