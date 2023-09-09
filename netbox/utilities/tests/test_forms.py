9f3c15201a156e4853770693fa976350ff808899
Per von Zweigbergk <pvz@pvz.pp.se>
2023-09-09 14:46:46 +0200

    Fix bogus test_invalid_range_alphanumeric  test
    Input errors should throw exceptions, not return an empty list. Should
    not significantly change external behaviour, in the old versions a "Field is
    required" message was an end result of these cases.

--- netbox/utilities/tests/test_forms.py
+++ netbox/utilities/tests/test_forms.py
@@ -275,8 +275,10 @@ class ExpandAlphanumeric(TestCase):
         self.assertEqual(sorted(expand_alphanumeric_pattern(input)), output)
 
     def test_invalid_range_alphanumeric(self):
-        self.assertEqual(sorted(expand_alphanumeric_pattern('r[9-a]a')), [])
-        self.assertEqual(sorted(expand_alphanumeric_pattern('r[a-9]a')), [])
+        with self.assertRaises(forms.ValidationError):
+            sorted(expand_alphanumeric_pattern('r[9-a]a'))
+        with self.assertRaises(forms.ValidationError):
+            sorted(expand_alphanumeric_pattern('r[a-9]a'))
 
     def test_invalid_range_bounds(self):
         with self.assertRaises(forms.ValidationError):
