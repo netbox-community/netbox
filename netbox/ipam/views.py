70e18021d1db17ba84244d5db6ee346c8b527db2
Jeremy Stretch <jstretch@netboxlabs.com>
2023-09-13 10:30:22 -0400

    Introduce get_table_ordering() utility to determine intended ordering given a request

--- netbox/ipam/views.py
+++ netbox/ipam/views.py
@@ -1,7 +1,6 @@
 from django.contrib.contenttypes.models import ContentType
 from django.db.models import F, Prefetch
 from django.db.models.expressions import RawSQL
-from django.db.models.functions import Round
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
 from django.utils.translation import gettext as _
@@ -11,6 +10,7 @@ from dcim.filtersets import InterfaceFilterSet
 from dcim.models import Interface, Site
 from netbox.views import generic
 from tenancy.views import ObjectContactsView
+from utilities.tables import get_table_ordering
 from utilities.utils import count_related
 from utilities.views import ViewTab, register_model_view
 from virtualization.filtersets import VMInterfaceFilterSet
@@ -606,12 +606,7 @@ class PrefixIPAddressesView(generic.ObjectChildrenView):
         return parent.get_child_ips().restrict(request.user, 'view').prefetch_related('vrf', 'tenant', 'tenant__group')
 
     def prep_table_data(self, request, queryset, parent):
-        # Check for presence of a q string, an ordering string, or user preferences ordering and the ordering string
-        # is blank
-        if not request.GET.get('q') and not request.GET.get('sort') and not (
-                request.user.is_authenticated and request.user.config.get(f'tables.IPAddressTable.ordering') and
-                not request.GET.get('sort') == ''
-        ):
+        if not get_table_ordering(request, self.table):
             return add_available_ipaddresses(parent.prefix, queryset, parent.is_pool)
         return queryset
 
