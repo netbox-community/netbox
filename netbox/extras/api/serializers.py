daef8546f67aa617ff269957d2b937e9cd56b472
Olivier Desnoë <desnoe@users.noreply.github.com>
2023-09-22 18:53:30 +0200

    Update serializers.py

--- netbox/extras/api/serializers.py
+++ netbox/extras/api/serializers.py
@@ -454,7 +454,7 @@ class ConfigTemplateSerializer(TaggableModelSerializer, ValidatedModelSerializer
         required=False
     )
     data_file = NestedDataFileSerializer(
-        read_only=False
+        required=False
     )
 
     class Meta:
