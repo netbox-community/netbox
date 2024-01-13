5bada6d3ed62780e13d28d44c77f5ce0b0918a2f
Arthur <worldnomad@gmail.com>
2024-01-12 16:19:18 -0800

    14729 rq table

---
+++ netbox/core/tables/tasks.py
@@ -0,0 +1,21 @@
+import django_tables2 as tables
+from django.utils.translation import gettext_lazy as _
+
+from netbox.tables import NetBoxTable, columns
+from ..models import Job
+
+
+class BackgroundTasksTable(tables.Table):
+    name = tables.Column()
+    jobs = tables.Column()
+    oldest_job_timestamp = tables.Column()
+    started_jobs = tables.Column()
+    deferred_jobs = tables.Column()
+    finished_jobs = tables.Column()
+    failed_jobs = tables.Column()
+    scheduled_jobs = tables.Column()
+    workers = tables.Column()
+    host = tables.Column(accessor="connection_kwargs__host")
+    port = tables.Column(accessor="connection_kwargs__port")
+    db = tables.Column(accessor="connection_kwargs__db")
+    pid = tables.Column(accessor="scheduler__pid")
