33075a09a131512b78d05b62e19f994d46476406
Daniel Sheppard <dans@dansheps.com>
2024-01-20 11:29:08 -0600

    Update netbox/extras/views.py
    Co-authored-by: Jeremy Stretch <jstretch@netboxlabs.com>

--- netbox/extras/views.py
+++ netbox/extras/views.py
@@ -1231,7 +1231,7 @@ class ScriptView(ContentTypePermissionRequiredMixin, View):
     def get(self, request, module, name):
         module = get_script_module(module, request)
         script = module.scripts[name]()
-        jobs = module.get_jobs(name)
+        jobs = module.get_jobs(script.class_name)
         form = script.as_form(initial=normalize_querydict(request.GET))
 
         # Look for a pending Job (use the latest one by creation timestamp)
