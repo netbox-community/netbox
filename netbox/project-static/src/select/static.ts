80b49aa6b2617a295c30105aa7420022ca1787df
Jeremy Stretch <jstretch@netboxlabs.com>
2024-02-09 14:34:11 -0500

    Improve rendering of color options

--- netbox/project-static/src/select/static.ts
+++ netbox/project-static/src/select/static.ts
@@ -18,7 +18,9 @@ export function initStaticSelects(): void {
 // Initialize color selection fields
 export function initColorSelects(): void {
   function renderColor(item: TomOption, escape: typeof escape_html) {
-    return `<div><span class="dropdown-item-indicator color-label" style="background-color: #${escape(item.value)}"></span> ${escape(item.text)}</div>`
+    return `<div><span class="dropdown-item-indicator color-label" style="background-color: #${escape(
+      item.value,
+    )}"></span> ${escape(item.text)}</div>`;
   }
 
   for (const select of getElements<HTMLSelectElement>('select.color-select')) {
