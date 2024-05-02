e164f049fac52a815523e1af044eff5c1a594d32
Jeremy Stretch <jstretch@netboxlabs.com>
2024-05-02 09:09:48 -0400

    Enable remove_button plugin only for multi-select fields

--- netbox/project-static/src/select/config.ts
+++ netbox/project-static/src/select/config.ts
@@ -3,22 +3,22 @@ interface PluginConfig {
 }
 
 export function getPlugins(element: HTMLSelectElement): object {
-  let plugins: PluginConfig = {}
+  const plugins: PluginConfig = {};
 
   // Enable "clear all" button
   plugins.clear_button = {
     html: (data: Dict) =>
       `<i class="mdi mdi-close-circle ${data.className}" title="${data.title}"></i>`,
-  }
+  };
 
   // Enable individual "remove" buttons for items on multi-select fields
   if (element.hasAttribute('multiple')) {
     plugins.remove_button = {
       title: 'Remove',
-    }
+    };
   }
 
   return {
     plugins: plugins,
-  }
+  };
 }
