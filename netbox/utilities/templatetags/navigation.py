a4789ba9c74c7f038c769b8b228306f8331e5b39
Jeremy Stretch <jstretch@netboxlabs.com>
2024-05-06 16:57:14 -0400

    Fixes #15977: Hide all admin menu items for non-authenticated users

--- netbox/utilities/templatetags/navigation.py
+++ netbox/utilities/templatetags/navigation.py
@@ -26,6 +26,8 @@ def nav(context):
         for group in menu.groups:
             items = []
             for item in group.items:
+                if item.auth_required and not user.is_authenticated:
+                    continue
                 if not user.has_perms(item.permissions):
                     continue
                 if item.staff_only and not user.is_staff:
