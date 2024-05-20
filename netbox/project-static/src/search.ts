8f67b9b64069ff3c189353ce865e0bfa69ed3b0b
Julio-Oliveira-Encora <julioh.oliveira@encora.com>
2024-05-20 16:15:49 -0300

    Addressed PR comments

--- netbox/project-static/src/search.ts
+++ netbox/project-static/src/search.ts
@@ -53,26 +53,23 @@ export function initQuickSearch(): void {
   const clearbtn = document.getElementById("quicksearch_clear") as HTMLAnchorElement;
   if (isTruthy(quicksearch)) {
     quicksearch.addEventListener("keyup", quickSearchEventHandler, {
-      passive: true,
+      passive: true
     });
     quicksearch.addEventListener("search", quickSearchEventHandler, {
-      passive: true,
+      passive: true
     });
     quicksearch.addEventListener("change", handleQuickSearchParams);
 
     if (isTruthy(clearbtn)) {
-      clearbtn.addEventListener(
-        "click",
-        async () => {
-          const search = new Event("search");
-          quicksearch.value = "";
+      clearbtn.addEventListener("click", async () => {
+          const search = new Event('search');
+          quicksearch.value = '';
           await new Promise(f => setTimeout(f, 100));
           quicksearch.dispatchEvent(search);
         },
         {
-          passive: true,
-        },
-      );
+          passive: true
+        })
     }
   }
 }
