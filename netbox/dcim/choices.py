d3e25f6bfa70821fcde9e0d69196602707542a2e
Julio-Oliveira-Encora <julioh.oliveira@encora.com>
2024-05-21 10:23:21 -0300

    Added 4G for Cellular choices.

--- netbox/dcim/choices.py
+++ netbox/dcim/choices.py
@@ -892,6 +892,7 @@ class InterfaceTypeChoices(ChoiceSet):
     TYPE_GSM = 'gsm'
     TYPE_CDMA = 'cdma'
     TYPE_LTE = 'lte'
+    TYPE_4G = '4g'
     TYPE_5G = '5g'
 
     # SONET
@@ -1061,6 +1062,7 @@ class InterfaceTypeChoices(ChoiceSet):
                 (TYPE_GSM, 'GSM'),
                 (TYPE_CDMA, 'CDMA'),
                 (TYPE_LTE, 'LTE'),
+                (TYPE_4G, '4G'),
                 (TYPE_5G, '5G'),
             )
         ),
