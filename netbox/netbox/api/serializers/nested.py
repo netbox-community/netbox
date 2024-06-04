fb1f72ea696fe456b3c6531cd7388aa0229959f5
Arthur <worldnomad@gmail.com>
2024-06-04 15:05:37 -0700

    15156 fix typo

--- netbox/netbox/api/serializers/nested.py
+++ netbox/netbox/api/serializers/nested.py
@@ -23,7 +23,7 @@ class WritableNestedSerializer(BaseModelSerializer):
 
 # Declared here for use by PrimaryModelSerializer, but should be imported from extras.api.nested_serializers
 class NestedTagSerializer(WritableNestedSerializer):
-    url = serializers.HyperlinkedIdentityField(view_name='extras-api:tag-detai')
+    url = serializers.HyperlinkedIdentityField(view_name='extras-api:tag-detail')
     display_url = serializers.HyperlinkedIdentityField(view_name='extras:tag')
 
     class Meta:
