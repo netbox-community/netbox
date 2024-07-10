017bcbbeba4d26611fec1fb12148e9c83878fc4e
Jeremy Stretch <jstretch@netboxlabs.com>
2024-07-10 08:58:43 -0400

    Set depth & quiet parameters only if using a remote URL

--- netbox/core/data_backends.py
+++ netbox/core/data_backends.py
@@ -84,9 +84,7 @@ class GitBackend(DataBackend):
         clone_args = {
             "branch": self.params.get('branch'),
             "config": self.config,
-            "depth": 1,
             "errstream": porcelain.NoneStream(),
-            "quiet": True,
         }
 
         if self.url_scheme in ('http', 'https'):
@@ -97,9 +95,9 @@ class GitBackend(DataBackend):
                         "password": self.params.get('password'),
                     }
                 )
-        elif not self.url_scheme:
-            clone_args["depth"] = None
-            del (clone_args["quiet"])
+        if self.url_scheme:
+            clone_args["quiet"] = True
+            clone_args["depth"] = 1
 
         logger.debug(f"Cloning git repo: {self.url}")
         try:
