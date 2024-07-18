6302a10a61aae4a10d4adf3aff44debe436c2cef
Jeremy Stretch <jstretch@netboxlabs.com>
2024-07-18 15:56:09 -0400

    Remove namespace

--- netbox/core/events.py
+++ netbox/core/events.py
@@ -1,6 +1,6 @@
 from django.utils.translation import gettext as _
 
-from netbox.events import *
+from netbox.events import Event, EVENT_TYPE_DANGER, EVENT_TYPE_SUCCESS, EVENT_TYPE_WARNING
 
 __all__ = (
     'JOB_COMPLETED',
@@ -12,24 +12,22 @@ __all__ = (
     'OBJECT_UPDATED',
 )
 
-NAMESPACE = 'netbox'
-
 # Object events
-OBJECT_CREATED = f'{NAMESPACE}.object_created'
-OBJECT_UPDATED = f'{NAMESPACE}.object_updated'
-OBJECT_DELETED = f'{NAMESPACE}.object_deleted'
+OBJECT_CREATED = 'object_created'
+OBJECT_UPDATED = 'object_updated'
+OBJECT_DELETED = 'object_deleted'
 
 # Job events
-JOB_STARTED = f'{NAMESPACE}.job_started'
-JOB_COMPLETED = f'{NAMESPACE}.job_completed'
-JOB_FAILED = f'{NAMESPACE}.job_failed'
-JOB_ERRORED = f'{NAMESPACE}.job_errored'
+JOB_STARTED = 'job_started'
+JOB_COMPLETED = 'job_completed'
+JOB_FAILED = 'job_failed'
+JOB_ERRORED = 'job_errored'
 
 # Register core events
-Event(name=OBJECT_CREATED, text=_('Object created')).register()
-Event(name=OBJECT_UPDATED, text=_('Object updated')).register()
-Event(name=OBJECT_DELETED, text=_('Object deleted')).register()
-Event(name=JOB_STARTED, text=_('Job started')).register()
-Event(name=JOB_COMPLETED, text=_('Job completed'), type=EVENT_TYPE_SUCCESS).register()
-Event(name=JOB_FAILED, text=_('Job failed'), type=EVENT_TYPE_WARNING).register()
-Event(name=JOB_ERRORED, text=_('Job errored'), type=EVENT_TYPE_DANGER).register()
+Event(OBJECT_CREATED, _('Object created')).register()
+Event(OBJECT_UPDATED, _('Object updated')).register()
+Event(OBJECT_DELETED, _('Object deleted')).register()
+Event(JOB_STARTED, _('Job started')).register()
+Event(JOB_COMPLETED, _('Job completed'), type=EVENT_TYPE_SUCCESS).register()
+Event(JOB_FAILED, _('Job failed'), type=EVENT_TYPE_WARNING).register()
+Event(JOB_ERRORED, _('Job errored'), type=EVENT_TYPE_DANGER).register()
