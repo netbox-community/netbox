c732a71230e119b45677a7a910370e3bff9e7486
Jeremy Stretch <jstretch@netboxlabs.com>
2024-10-10 08:51:07 -0400

    Correct handling of GenericForeignKey in PrefixForm

--- netbox/ipam/forms/model_forms.py
+++ netbox/ipam/forms/model_forms.py
@@ -207,7 +207,7 @@ class PrefixForm(TenancyForm, NetBoxModelForm):
         required=False,
         label=_('Scope type')
     )
-    scope_id = DynamicModelChoiceField(
+    scope = DynamicModelChoiceField(
         label=_('Scope'),
         queryset=Site.objects.none(),  # Initial queryset
         required=False,
@@ -234,7 +234,7 @@ class PrefixForm(TenancyForm, NetBoxModelForm):
         FieldSet(
             'prefix', 'status', 'vrf', 'role', 'is_pool', 'mark_utilized', 'description', 'tags', name=_('Prefix')
         ),
-        FieldSet('scope_type', 'scope_id', name=_('Scope')),
+        FieldSet('scope_type', 'scope', name=_('Scope')),
         FieldSet('vlan', name=_('VLAN Assignment')),
         FieldSet('tenant_group', 'tenant', name=_('Tenancy')),
     )
@@ -242,8 +242,8 @@ class PrefixForm(TenancyForm, NetBoxModelForm):
     class Meta:
         model = Prefix
         fields = [
-            'prefix', 'vrf', 'vlan', 'status', 'role', 'is_pool', 'mark_utilized', 'scope_type', 'scope_id',
-            'tenant_group', 'tenant', 'description', 'comments', 'tags',
+            'prefix', 'vrf', 'vlan', 'status', 'role', 'is_pool', 'mark_utilized', 'scope_type', 'tenant_group',
+            'tenant', 'description', 'comments', 'tags',
         ]
 
     def __init__(self, *args, **kwargs):
@@ -260,21 +260,21 @@ class PrefixForm(TenancyForm, NetBoxModelForm):
             try:
                 scope_type = ContentType.objects.get(pk=scope_type_id)
                 model = scope_type.model_class()
-                self.fields['scope_id'].queryset = model.objects.all()
-                self.fields['scope_id'].widget.attrs['selector'] = model._meta.label_lower
-                self.fields['scope_id'].disabled = False
-                self.fields['scope_id'].label = _(bettertitle(model._meta.verbose_name))
+                self.fields['scope'].queryset = model.objects.all()
+                self.fields['scope'].widget.attrs['selector'] = model._meta.label_lower
+                self.fields['scope'].disabled = False
+                self.fields['scope'].label = _(bettertitle(model._meta.verbose_name))
             except ObjectDoesNotExist:
                 pass
 
             if self.instance and scope_type_id != self.instance.scope_type_id:
-                self.initial['scope_id'] = None
+                self.initial['scope'] = None
 
     def clean(self):
         super().clean()
 
         # Assign the selected scope (if any)
-        self.instance.scope = self.cleaned_data.get('scope_id')
+        self.instance.scope = self.cleaned_data.get('scope')
 
 
 class IPRangeForm(TenancyForm, NetBoxModelForm):
