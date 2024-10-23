dcb3c7c113670dfc5129d78ba90f3577809e285a
Arthur Hanson <worldnomad@gmail.com>
2024-10-23 12:50:12 -0700

    7699 Serializer

--- netbox/virtualization/api/serializers_/clusters.py
+++ netbox/virtualization/api/serializers_/clusters.py
@@ -1,9 +1,15 @@
+from django.contrib.contenttypes.models import ContentType
+from drf_spectacular.utils import extend_schema_field
+from rest_framework import serializers
+
 from dcim.api.serializers_.sites import SiteSerializer
-from netbox.api.fields import ChoiceField, RelatedObjectCountField
+from netbox.api.fields import ChoiceField, ContentTypeField, RelatedObjectCountField
 from netbox.api.serializers import NetBoxModelSerializer
 from tenancy.api.serializers_.tenants import TenantSerializer
 from virtualization.choices import *
+from virtualization.constants import CLUSTER_SCOPE_TYPES
 from virtualization.models import Cluster, ClusterGroup, ClusterType
+from utilities.api import get_serializer_for_model
 
 __all__ = (
     'ClusterGroupSerializer',
@@ -46,6 +52,16 @@ class ClusterSerializer(NetBoxModelSerializer):
     status = ChoiceField(choices=ClusterStatusChoices, required=False)
     tenant = TenantSerializer(nested=True, required=False, allow_null=True)
     site = SiteSerializer(nested=True, required=False, allow_null=True, default=None)
+    scope_type = ContentTypeField(
+        queryset=ContentType.objects.filter(
+            model__in=CLUSTER_SCOPE_TYPES
+        ),
+        allow_null=True,
+        required=False,
+        default=None
+    )
+    scope_id = serializers.IntegerField(allow_null=True, required=False, default=None)
+    scope = serializers.SerializerMethodField(read_only=True)
 
     # Related object counts
     device_count = RelatedObjectCountField('devices')
@@ -54,8 +70,18 @@ class ClusterSerializer(NetBoxModelSerializer):
     class Meta:
         model = Cluster
         fields = [
-            'id', 'url', 'display_url', 'display', 'name', 'type', 'group', 'status', 'tenant', 'site',
+            'id', 'url', 'display_url', 'display', 'name', 'type', 'group', 'status', 'tenant', 'scope_type', 'scope_id', 'scope',
             'description', 'comments', 'tags', 'custom_fields', 'created', 'last_updated', 'device_count',
             'virtualmachine_count',
         ]
         brief_fields = ('id', 'url', 'display', 'name', 'description', 'virtualmachine_count')
+
+    @extend_schema_field(serializers.JSONField(allow_null=True))
+    def get_scope(self, obj):
+        if obj.scope_id is None:
+            return None
+        serializer = get_serializer_for_model(obj.scope)
+        context = {'request': self.context['request']}
+        return serializer(obj.scope, nested=True, context=context).data
+
+
