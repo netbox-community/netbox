8b0af50019fa5f197d5901d95190111ed8ea215c
Alexander Haase <ahaase@alexhaase.de>
2025-02-15 18:11:37 +0100

    Add URL test case for regular models

--- netbox/netbox/tests/test_model.py
+++ netbox/netbox/tests/test_model.py
@@ -1,9 +1,16 @@
 from django.test import TestCase
 
+from core.models import ObjectChange
 from netbox.tests.dummy_plugin.models import DummyModel
 
 
 class ModelTest(TestCase):
+
+    def test_get_absolute_url(self):
+        m = ObjectChange()
+        m.pk = 123
+
+        self.assertEqual(m.get_absolute_url(), f'/core/changelog/{m.pk}/')
 
     def test_absolute_url(self):
         m = DummyModel(name='Foo')
