9df8e8249291e9b07e7b79ca765126ac20a9999d
Arthur <worldnomad@gmail.com>
2025-03-20 11:25:34 -0700

    16224 review changes

--- netbox/dcim/migrations/0203_add_rack_outer_height.py
+++ netbox/dcim/migrations/0203_add_rack_outer_height.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('dcim', '0201_add_power_outlet_status'),
+        ('dcim', '0202_location_comments_region_comments_sitegroup_comments'),
     ]
 
     operations = [
