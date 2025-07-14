21a840c32e6e1bc40b11c6e7e34fbf143ea24033
Jeremy Stretch <jstretch@netboxlabs.com>
2025-07-14 09:52:50 -0400

    Closes #19816: Implement a logging mechanism for background jobs (#19838)
    * Initial work on #19816
    
    * Use TZ-aware timestamps
    
    * Deserialize JobLogEntry timestamp
    
    * Repurpose RQJobStatusColumn to display job entry level badges
    
    * Misc cleanup
    
    * Test logging
    
    * Refactor HTML templates
    
    * Update documentation

--- netbox/netbox/jobs.py
+++ netbox/netbox/jobs.py
@@ -38,6 +38,19 @@ def system_job(interval):
     return _wrapper
 
 
+class JobLogHandler(logging.Handler):
+    """
+    A logging handler which records entries on a Job.
+    """
+    def __init__(self, job, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.job = job
+
+    def emit(self, record):
+        # Enter the record in the log of the associated Job
+        self.job.log(record)
+
+
 class JobRunner(ABC):
     """
     Background Job helper class.
@@ -55,6 +68,11 @@ class JobRunner(ABC):
             job: The specific `Job` this `JobRunner` is executing.
         """
         self.job = job
+
+        # Initiate the system logger
+        self.logger = logging.getLogger(f"netbox.jobs.{self.__class__.__name__}")
+        self.logger.setLevel(logging.DEBUG)
+        self.logger.addHandler(JobLogHandler(job))
 
     @classproperty
     def name(cls):
