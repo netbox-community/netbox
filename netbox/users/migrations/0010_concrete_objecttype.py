221ff7e8a7bb99a40941e5e368170bd1925183f0
Jeremy Stretch <jstretch@netboxlabs.com>
2025-07-22 10:11:11 -0400

    Convert ObjectType to a concrete child model of ContentType

---
+++ netbox/users/migrations/0010_concrete_objecttype.py
@@ -0,0 +1,17 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0016_concrete_objecttype'),
+        ('users', '0009_update_group_perms'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='ObjectPermission',
+            name='object_types',
+            field=models.ManyToManyField(related_name='object_permissions', to='core.objecttype'),
+        ),
+    ]
