3589f73da1f8dd1dc38b3bbb7cf0bf1d7209e2bc
Jeremy Stretch <jstretch@netboxlabs.com>
2025-07-24 09:17:58 -0400

    Don't automatically create ObjectTypes when a ContentType is created

--- netbox/core/signals.py
+++ netbox/core/signals.py
@@ -2,7 +2,6 @@ import logging
 
 from django.contrib.contenttypes.models import ContentType
 from django.core.exceptions import ObjectDoesNotExist, ValidationError
-from django.db import ProgrammingError
 from django.db.models.fields.reverse_related import ManyToManyRel, ManyToOneRel
 from django.db.models.signals import m2m_changed, post_migrate, post_save, pre_delete
 from django.dispatch import receiver, Signal
@@ -70,21 +69,6 @@ def update_object_types(sender, **kwargs):
                 public=is_public,
                 features=features,
             )
-
-
-@receiver(post_save, sender=ContentType)
-def create_object_type(sender, instance, created, **kwargs):
-    if created:
-        model = instance.model_class()
-        try:
-            ObjectType.objects.create(
-                contenttype_ptr=instance,
-                public=model_is_public(model),
-                features=get_model_features(model),
-            )
-        except ProgrammingError:
-            # Will fail during migrations if ObjectType hasn't been created yet
-            pass
 
 
 #
