f3155973f6f9620f8147243f53917a08e9bfd369
Jeremy Stretch <jstretch@netboxlabs.com>
2025-07-28 15:26:16 -0400

    Subclass django_prometheus middleware to capture REST API metrics

---
+++ netbox/netbox/metrics.py
@@ -0,0 +1,32 @@
+from django_prometheus.conf import NAMESPACE
+from django_prometheus import middleware
+from prometheus_client import Counter
+
+__all__ = (
+    'Metrics',
+)
+
+
+class Metrics(middleware.Metrics):
+    """
+    Expand the stock Metrics class from django_prometheus to add our own counters.
+    """
+
+    def register(self):
+        super().register()
+
+        # REST API metrics
+        self.rest_api_requests = self.register_metric(
+            Counter,
+            "rest_api_requests_total_by_method",
+            "Count of total REST API requests by method",
+            ["method"],
+            namespace=NAMESPACE,
+        )
+        self.rest_api_requests_by_view_method = self.register_metric(
+            Counter,
+            "django_http_requests_total_by_view_method",
+            "Count of REST API requests by view & method",
+            ["view", "method"],
+            namespace=NAMESPACE,
+        )
