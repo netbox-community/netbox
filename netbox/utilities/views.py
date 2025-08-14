a9e2872d0f6208d00c7a778c678ebc90d8e26305
Daniel Sheppard <dans@dansheps.com>
2025-08-13 22:09:21 -0500

    Update per discussion

--- netbox/utilities/views.py
+++ netbox/utilities/views.py
@@ -49,10 +49,7 @@ class TokenConditionalLoginRequiredMixin(ConditionalLoginRequiredMixin):
             auth_info = authenticator.authenticate(request)
             if auth_info is not None:
                 request.user = auth_info[0]  # User object
-                try:
-                    request.auth = auth_info[1]
-                except IndexError:
-                    pass
+                request.auth = auth_info[1]
 
         return super().dispatch(request, *args, **kwargs)
 
