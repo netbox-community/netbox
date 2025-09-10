16952434b128a2ce0c5add9c1d183bc5781d04e0
Jeremy Stretch <jstretch@netboxlabs.com>
2025-09-10 09:34:12 -0400

    Introduce IsSuperuser API permission to replace IsAdminUser

--- netbox/utilities/api.py
+++ netbox/utilities/api.py
@@ -6,6 +6,7 @@ from django.db.models.fields.related import ManyToOneRel, RelatedField
 from django.urls import reverse
 from django.utils.module_loading import import_string
 from django.utils.translation import gettext_lazy as _
+from rest_framework.permissions import BasePermission
 from rest_framework.serializers import Serializer
 from rest_framework.views import get_view_name as drf_get_view_name
 
@@ -16,6 +17,7 @@ from .query import count_related, dict_to_filter_params
 from .string import title
 
 __all__ = (
+    'IsSuperuser',
     'get_annotations_for_serializer',
     'get_graphql_type_for_model',
     'get_prefetches_for_serializer',
@@ -25,6 +27,14 @@ __all__ = (
     'is_api_request',
     'is_graphql_request',
 )
+
+
+class IsSuperuser(BasePermission):
+    """
+    Allows access only to superusers.
+    """
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.is_superuser)
 
 
 def get_serializer_for_model(model, prefix=''):
