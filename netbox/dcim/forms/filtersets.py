5913ea8a8baf15e413457b79a837a029aefc4f03
Jason Novinger <jnovinger@gmail.com>
2025-10-26 23:21:28 -0500

    Fix import order

--- netbox/dcim/forms/filtersets.py
+++ netbox/dcim/forms/filtersets.py
@@ -3,7 +3,7 @@ from django.utils.translation import gettext_lazy as _
 
 from dcim.choices import *
 from dcim.constants import *
-from dcim.filtersets import DeviceFilterSet, RackFilterSet, PowerOutletFilterSet
+from dcim.filtersets import DeviceFilterSet, PowerOutletFilterSet, RackFilterSet
 from dcim.models import *
 from extras.forms import LocalConfigContextFilterForm
 from extras.models import ConfigTemplate
