35fb62b560a888869029e6dabb52daec69348bf6
Jeremy Stretch <jstretch@netboxlabs.com>
2025-12-18 10:36:23 -0500

    Fixes #20997: Enable creating permissions for the Owner model

--- netbox/users/constants.py
+++ netbox/users/constants.py
@@ -5,7 +5,7 @@ from django.db.models import Q
 
 OBJECTPERMISSION_OBJECT_TYPES = Q(
     ~Q(app_label__in=['account', 'admin', 'auth', 'contenttypes', 'sessions', 'taggit', 'users']) |
-    Q(app_label='users', model__in=['objectpermission', 'token', 'group', 'user'])
+    Q(app_label='users', model__in=['objectpermission', 'token', 'group', 'user', 'owner'])
 )
 
 CONSTRAINT_TOKEN_USER = '$user'
