07d8157ccd9b8182f983e493147c5ba001bf2d76
Mark Coleman <mrmrcoleman@gmail.com>
2025-12-20 11:02:36 +0100

    Fix missing owner field in DeviceWithConfigContextSerializer
    Fixes: https://github.com/netbox-community/netbox/issues/21022

--- netbox/dcim/api/serializers_/devices.py
+++ netbox/dcim/api/serializers_/devices.py
@@ -111,7 +111,7 @@ class DeviceWithConfigContextSerializer(DeviceSerializer):
             'id', 'url', 'display_url', 'display', 'name', 'device_type', 'role', 'tenant', 'platform', 'serial',
             'asset_tag', 'site', 'location', 'rack', 'position', 'face', 'latitude', 'longitude', 'parent_device',
             'status', 'airflow', 'primary_ip', 'primary_ip4', 'primary_ip6', 'oob_ip', 'cluster', 'virtual_chassis',
-            'vc_position', 'vc_priority', 'description', 'comments', 'config_template', 'config_context',
+            'vc_position', 'vc_priority', 'description', 'owner', 'comments', 'config_template', 'config_context',
             'local_context_data', 'tags', 'custom_fields', 'created', 'last_updated', 'console_port_count',
             'console_server_port_count', 'power_port_count', 'power_outlet_count', 'interface_count',
             'front_port_count', 'rear_port_count', 'device_bay_count', 'module_bay_count', 'inventory_item_count',
