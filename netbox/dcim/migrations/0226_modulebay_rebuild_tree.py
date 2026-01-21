6a5aced4bc1b58ed54682ff06df4d29f0abf7445
Arthur <worldnomad@gmail.com>
2026-01-21 12:28:01 -0800

    fix migration

--- netbox/dcim/migrations/0226_modulebay_rebuild_tree.py
+++ netbox/dcim/migrations/0226_modulebay_rebuild_tree.py
@@ -1,6 +1,4 @@
 from django.db import migrations
-import mptt
-import mptt.managers
 
 
 def rebuild_mptt(apps, schema_editor):
@@ -8,12 +6,8 @@ def rebuild_mptt(apps, schema_editor):
     Rebuild the MPTT tree for ModuleBay to apply new ordering by 'name'
     instead of 'module'.
     """
-    manager = mptt.managers.TreeManager()
     ModuleBay = apps.get_model('dcim', 'ModuleBay')
-    manager.model = ModuleBay
-    mptt.register(ModuleBay)
-    manager.contribute_to_class(ModuleBay, 'objects')
-    manager.rebuild()
+    ModuleBay.objects.rebuild()
 
 
 class Migration(migrations.Migration):
