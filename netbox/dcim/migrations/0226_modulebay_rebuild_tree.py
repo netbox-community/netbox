ef0bc180951a38fa1b9828755eb0e0cbe582b6c6
Arthur <worldnomad@gmail.com>
2026-01-21 12:47:16 -0800

    fix migration

--- netbox/dcim/migrations/0226_modulebay_rebuild_tree.py
+++ netbox/dcim/migrations/0226_modulebay_rebuild_tree.py
@@ -5,10 +5,17 @@ import mptt.managers
 
 def rebuild_mptt(apps, schema_editor):
     """
-    Rebuild the MPPT tree for ModuleBay to apply new ordering by 'name'
+    Rebuild the MPTT tree for ModuleBay to apply new ordering by 'name'
     instead of 'module'.
     """
     ModuleBay = apps.get_model('dcim', 'ModuleBay')
+
+    # Set MPTTMeta with the correct order_insertion_by
+    class MPTTMeta:
+        order_insertion_by = ('module', 'name',)
+
+    ModuleBay.MPTTMeta = MPTTMeta
+
     manager = mptt.managers.TreeManager()
     manager.model = ModuleBay
     mptt.register(ModuleBay)
