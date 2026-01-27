731c0a9e089f790f07ea222c5e4f8e0a44b57891
Arthur <worldnomad@gmail.com>
2026-01-27 09:30:48 -0800

    Add queue name to Job

--- netbox/core/models/jobs.py
+++ netbox/core/models/jobs.py
@@ -112,6 +112,12 @@ class Job(models.Model):
         verbose_name=_('job ID'),
         unique=True
     )
+    queue_name = models.CharField(
+        verbose_name=_('queue name'),
+        max_length=100,
+        blank=True,
+        help_text=_('Name of the queue in which this job was enqueued')
+    )
     log_entries = ArrayField(
         verbose_name=_('log entries'),
         base_field=models.JSONField(
@@ -181,7 +187,8 @@ class Job(models.Model):
     def delete(self, *args, **kwargs):
         super().delete(*args, **kwargs)
 
-        rq_queue_name = get_queue_for_model(self.object_type.model if self.object_type else None)
+        # Use the stored queue name, or fall back to get_queue_for_model for legacy jobs
+        rq_queue_name = self.queue_name or get_queue_for_model(self.object_type.model if self.object_type else None)
         queue = django_rq.get_queue(rq_queue_name)
         job = queue.fetch_job(str(self.job_id))
 
@@ -288,7 +295,8 @@ class Job(models.Model):
             scheduled=schedule_at,
             interval=interval,
             user=user,
-            job_id=uuid.uuid4()
+            job_id=uuid.uuid4(),
+            queue_name=rq_queue_name
         )
         job.full_clean()
         job.save()
