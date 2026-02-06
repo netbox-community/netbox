97847662ac17adbdbcf1e21136635e7fd2c48e2d
Arthur <worldnomad@gmail.com>
2026-02-06 10:34:27 -0800

    cleanup

--- netbox/netbox/settings.py
+++ netbox/netbox/settings.py
@@ -401,8 +401,6 @@ if CACHING_REDIS_SENTINELS:
     CACHES['default']['LOCATION'] = f'{CACHING_REDIS_PROTO}://{CACHING_REDIS_SENTINEL_SERVICE}/{CACHING_REDIS_DATABASE}'
     CACHES['default']['OPTIONS']['CLIENT_CLASS'] = 'django_redis.client.SentinelClient'
     CACHES['default']['OPTIONS']['SENTINELS'] = CACHING_REDIS_SENTINELS
-
-# Backwards compatibility: map existing SSL parameters
 if CACHING_REDIS_SKIP_TLS_VERIFY:
     CACHES['default']['OPTIONS'].setdefault('CONNECTION_POOL_KWARGS', {})
     CACHES['default']['OPTIONS']['CONNECTION_POOL_KWARGS']['ssl_cert_reqs'] = False
