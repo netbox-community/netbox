2cfecd7052dd120452cf5a09ed98c29f7c363cb9
Jason Novinger <jnovinger@gmail.com>
2026-02-20 13:45:52 -0600

    Add ModelAction and register_model_actions() API for custom permission actions

--- netbox/netbox/registry.py
+++ netbox/netbox/registry.py
@@ -28,6 +28,7 @@ registry = Registry({
     'denormalized_fields': collections.defaultdict(list),
     'event_types': dict(),
     'filtersets': dict(),
+    'model_actions': collections.defaultdict(list),
     'model_features': dict(),
     'models': collections.defaultdict(set),
     'plugins': dict(),
