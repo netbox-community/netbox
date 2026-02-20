02b85765d927b9b33fb13f46014108db340130f6
Jason Novinger <jnovinger@gmail.com>
2026-02-20 13:50:40 -0600

    Register custom actions for DataSource, Device, and VirtualMachine

--- netbox/dcim/apps.py
+++ netbox/dcim/apps.py
@@ -8,14 +8,22 @@ class DCIMConfig(AppConfig):
     verbose_name = "DCIM"
 
     def ready(self):
+        from django.utils.translation import gettext as _
+
         from netbox.models.features import register_models
         from utilities.counters import connect_counters
+        from utilities.permissions import ModelAction, register_model_actions
 
         from . import search, signals  # noqa: F401
         from .models import CableTermination, Device, DeviceType, ModuleType, RackType, VirtualChassis
 
         # Register models
         register_models(*self.get_models())
+
+        # Register custom permission actions
+        register_model_actions(Device, [
+            ModelAction('render_config', help_text=_('Render device configuration')),
+        ])
 
         # Register denormalized fields
         denormalized.register(CableTermination, '_device', {
--- netbox/virtualization/apps.py
+++ netbox/virtualization/apps.py
@@ -5,8 +5,11 @@ class VirtualizationConfig(AppConfig):
     name = 'virtualization'
 
     def ready(self):
+        from django.utils.translation import gettext as _
+
         from netbox.models.features import register_models
         from utilities.counters import connect_counters
+        from utilities.permissions import ModelAction, register_model_actions
 
         from . import search, signals  # noqa: F401
         from .models import VirtualMachine
@@ -16,3 +19,8 @@ class VirtualizationConfig(AppConfig):
 
         # Register counters
         connect_counters(VirtualMachine)
+
+        # Register custom permission actions
+        register_model_actions(VirtualMachine, [
+            ModelAction('render_config', help_text=_('Render VM configuration')),
+        ])
