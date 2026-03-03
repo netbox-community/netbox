3b0b95c2657be4c6d3deea824a08bb393c2c2759
Arthur Hanson <worldnomad@gmail.com>
2026-03-03 05:01:04 -0800

    Closes #21550: Call snapshot() before saving related objects (#21551)
    Add missing pre-change `snapshot()` calls in views/forms before updating
    and saving related objects (device bays, virtual chassis members, and
    bulk-import primary MAC/IP assignments), so changelog entries include
    pre-change data.

--- netbox/dcim/views.py
+++ netbox/dcim/views.py
@@ -2733,6 +2733,7 @@ class DeviceBulkImportView(generic.BulkImportView):
         # For child devices, save the reverse relation to the parent device bay
         if parent_bay:
             device_bay = parent_bay
+            device_bay.snapshot()
             device_bay.installed_device = obj
             device_bay.save()
 
@@ -4099,6 +4100,7 @@ class VirtualChassisEditView(ObjectPermissionRequiredMixin, GetReturnURLMixin, V
                 members = formset.save(commit=False)
                 devices = Device.objects.filter(pk__in=[m.pk for m in members])
                 for device in devices:
+                    device.snapshot()
                     device.vc_position = None
                     device.save()
                 for member in members:
