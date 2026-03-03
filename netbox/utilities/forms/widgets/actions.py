7541554d367e3e616ea7694833c8f54ae62b91fd
Jason Novinger <jnovinger@gmail.com>
2026-03-03 10:29:02 -0600

    Refine registered actions widget UI
    - Use verbose labels (App | Model) for action group headers
    - Simplify template layout with h5 headers instead of cards
    - Consolidate Standard/Custom/Additional Actions into single Actions fieldset

--- netbox/utilities/forms/widgets/actions.py
+++ netbox/utilities/forms/widgets/actions.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.apps import apps
 
 __all__ = (
     'RegisteredActionsWidget',
@@ -18,6 +19,22 @@ class RegisteredActionsWidget(forms.CheckboxSelectMultiple):
 
     def get_context(self, name, value, attrs):
         context = super().get_context(name, value, attrs)
-        context['widget']['model_actions'] = self.model_actions
+
+        # Build model_actions with labels for v2 template
+        model_actions_with_labels = {}
+        for model_key, actions in self.model_actions.items():
+            app_label, model_name = model_key.split('.')
+            try:
+                model = apps.get_model(app_label, model_name)
+                app_config = apps.get_app_config(app_label)
+                label = f"{app_config.verbose_name} | {model._meta.verbose_name.title()}"
+            except LookupError:
+                label = model_key
+            model_actions_with_labels[model_key] = {
+                'label': label,
+                'actions': actions,
+            }
+
+        context['widget']['model_actions'] = model_actions_with_labels
         context['widget']['value'] = value or []
         return context
