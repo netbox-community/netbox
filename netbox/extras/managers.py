7ad916c65f0e151aa4b8b10cac64a7d962cdd771
Arthur <worldnomad@gmail.com>
2026-03-06 10:26:45 -0800

    review changes

--- netbox/extras/managers.py
+++ netbox/extras/managers.py
@@ -13,20 +13,22 @@ class NetBoxTaggableManager(_TaggableManager):
     single bulk_create() call, reducing SQL queries from O(N) to O(1) when assigning tags.
     """
 
+    @require_instance_manager
     def add(self, *tags, through_defaults=None, tag_kwargs=None, **kwargs):
         self._remove_prefetched_objects()
         if tag_kwargs is None:
             tag_kwargs = {}
+        db = router.db_for_write(self.through, instance=self.instance)
 
         tag_objs = self._to_tag_model_instances(tags, tag_kwargs)
         new_ids = {t.pk for t in tag_objs}
 
         # Determine which tags are not already assigned to this object
-        db = router.db_for_write(self.through, instance=self.instance)
+        lookup = self._lookup_kwargs()
         vals = set(
             self.through._default_manager.using(db)
             .values_list("tag_id", flat=True)
-            .filter(**self._lookup_kwargs(), tag_id__in=new_ids)
+            .filter(**(lookup), tag_id__in=new_ids)
         )
         new_ids -= vals
 
@@ -44,7 +46,6 @@ class NetBoxTaggableManager(_TaggableManager):
         )
 
         # Use a single bulk INSERT instead of one get_or_create per tag.
-        lookup = self._lookup_kwargs()
         self.through._default_manager.using(db).bulk_create(
             [
                 self.through(tag=tag, **lookup, **(through_defaults or {}))
