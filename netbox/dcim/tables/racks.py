5cf241906d235c5ca7b28acf976bde199e53e163
Brian Tiemann <btman@mac.com>
2026-03-24 20:21:46 -0400

    Add 'created' back into fields

--- netbox/dcim/tables/racks.py
+++ netbox/dcim/tables/racks.py
@@ -255,8 +255,7 @@ class RackReservationTable(TenancyColumnsMixin, PrimaryModelTable):
         model = RackReservation
         fields = (
             'pk', 'id', 'reservation', 'site', 'location', 'group', 'rack', 'unit_list', 'unit_count', 'status',
-            'user', 'tenant',
-            'tenant_group', 'description', 'comments', 'tags', 'actions', 'last_updated',
+            'user', 'created', 'tenant', 'tenant_group', 'description', 'comments', 'tags', 'actions', 'last_updated',
         )
         default_columns = (
             'pk', 'reservation', 'site', 'rack', 'unit_list', 'unit_count', 'status', 'user', 'description',
