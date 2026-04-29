c6636945d32cf886adb3663a4f7028e4e98bffb3
Jeremy Stretch <jstretch@netboxlabs.com>
2026-04-29 08:47:15 -0400

    Add a test for null coercion on clean()

--- netbox/dcim/tests/test_models.py
+++ netbox/dcim/tests/test_models.py
@@ -638,11 +638,26 @@ class DeviceTestCase(TestCase):
         device2.full_clean()
         device2.save()
 
-    def test_empty_asset_tag_coerced_to_null(self):
+    def test_empty_asset_tag_coerced_to_null_on_clean(self):
         """
         An empty string assigned to a unique nullable CharField (e.g. asset_tag) must be coerced
         to None on save so that multiple objects can be saved without violating the unique
-        constraint.
+        constraint. Test that this is done on clean().
+        """
+        common_kwargs = {
+            'site': Site.objects.first(),
+            'device_type': DeviceType.objects.first(),
+            'role': DeviceRole.objects.first(),
+        }
+        device1 = Device(name='Device 1', asset_tag='', **common_kwargs)
+        device1.clean()
+        self.assertIsNone(device1.asset_tag)
+
+    def test_empty_asset_tag_coerced_to_null_on_save(self):
+        """
+        An empty string assigned to a unique nullable CharField (e.g. asset_tag) must be coerced
+        to None on save so that multiple objects can be saved without violating the unique
+        constraint. Test that this is done on save().
         """
         common_kwargs = {
             'site': Site.objects.first(),
