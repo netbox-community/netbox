09cd3f2dfdce22ae486543e4052dd2e2df5b0a36
Jeremy Stretch <jstretch@netboxlabs.com>
2026-08-05 14:49:50 -0400

    Populate execution_time for existing jobs

--- netbox/core/migrations/0025_add_job_execution_time.py
+++ netbox/core/migrations/0025_add_job_execution_time.py
@@ -1,4 +1,15 @@
 from django.db import migrations, models
+from django.db.models import DurationField, ExpressionWrapper, F
+
+
+def populate_execution_time(apps, schema_editor):
+    """
+    Populate execution_time for existing jobs which have both a start and completion time recorded.
+    """
+    Job = apps.get_model("core", "Job")
+    Job.objects.filter(started__isnull=False, completed__isnull=False).update(
+        execution_time=ExpressionWrapper(F("completed") - F("started"), output_field=DurationField())
+    )
 
 
 class Migration(migrations.Migration):
@@ -12,5 +23,9 @@ class Migration(migrations.Migration):
             model_name="job",
             name="execution_time",
             field=models.DurationField(blank=True, editable=False, null=True),
+        ),
+        migrations.RunPython(
+            code=populate_execution_time,
+            reverse_code=migrations.RunPython.noop,
         ),
     ]
