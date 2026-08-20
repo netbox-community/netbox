8aa5139e49fc42ec064a40e16abdee3660a839d6
Piyush Bhakuni <bhakunipiyush23@gmail.com>
2026-08-20 11:00:37 +0530

    dcim: Add HPE Synergy Interconnect Link as proprietary stacking interface type
    Closes #22660
    
    Adds TYPE_HPE_SYNERGY_INTERCONNECT = 'hpe-synergy-interconnect-link' to
    InterfaceTypeChoices as a proprietary stacking link, placed alongside
    other vendor stacking types (Extreme SummitStack series).
    
    HPE Synergy composable infrastructure uses a proprietary 300Gb
    Interconnect Link in QSFP-DD form factor to stack Synergy Frame Link
    Modules. This is not a standardized IEEE 802.3 rate, so it is
    categorized as a proprietary stacking link rather than a 300GBase-X
    Ethernet type.

--- netbox/dcim/choices.py
+++ netbox/dcim/choices.py
@@ -1033,7 +1033,6 @@ class InterfaceTypeChoices(ChoiceSet):
     TYPE_200GE_CFP2 = '200gbase-x-cfp2'
     TYPE_200GE_QSFP56 = '200gbase-x-qsfp56'
     TYPE_200GE_QSFP_DD = '200gbase-x-qsfpdd'
-    TYPE_300GE_QSFP_DD = '300gbase-x-qsfpdd'
     TYPE_400GE_CFP2 = '400gbase-x-cfp2'
     TYPE_400GE_QSFP112 = '400gbase-x-qsfp112'
     TYPE_400GE_QSFP_DD = '400gbase-x-qsfpdd'
@@ -1153,6 +1152,7 @@ class InterfaceTypeChoices(ChoiceSet):
     TYPE_SUMMITSTACK128 = 'extreme-summitstack-128'
     TYPE_SUMMITSTACK256 = 'extreme-summitstack-256'
     TYPE_SUMMITSTACK512 = 'extreme-summitstack-512'
+    TYPE_HPE_SYNERGY_INTERCONNECT = 'hpe-synergy-interconnect-link'
 
     # Other
     TYPE_OTHER = 'other'
@@ -1344,7 +1344,6 @@ class InterfaceTypeChoices(ChoiceSet):
                 (TYPE_200GE_CFP2, 'CFP2 (200GE)'),
                 (TYPE_200GE_QSFP56, 'QSFP56 (200GE)'),
                 (TYPE_200GE_QSFP_DD, 'QSFP-DD (200GE)'),
-                (TYPE_300GE_QSFP_DD, 'QSFP-DD (300GE)'),
                 (TYPE_400GE_QSFP112, 'QSFP112 (400GE)'),
                 (TYPE_400GE_QSFP_DD, 'QSFP-DD (400GE)'),
                 (TYPE_400GE_CDFP, 'CDFP (400GE)'),
@@ -1497,6 +1496,7 @@ class InterfaceTypeChoices(ChoiceSet):
                 (TYPE_SUMMITSTACK128, 'Extreme SummitStack-128'),
                 (TYPE_SUMMITSTACK256, 'Extreme SummitStack-256'),
                 (TYPE_SUMMITSTACK512, 'Extreme SummitStack-512'),
+                (TYPE_HPE_SYNERGY_INTERCONNECT, 'HPE Synergy Interconnect Link'),
             )
         ),
         (
